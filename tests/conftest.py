60ce1d1ff68bef127043c76e9f2b55175cb51bdd
Chris Hronek <pohek321@gmail.com>
2022-12-14 18:41:48 -0700

    Add cicd workflows and supporting tests

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+def pytest_addoption(parser):
+    parser.addoption("--package", action="store")
+
+@pytest.fixture(scope='session')
+def package(request):
+    package_value = request.config.option.package
+    if package_value is None:
+        pytest.skip()
+    return package_value
---
+++ tests/validation_tests.py
@@ -0,0 +1,12 @@
+def test_version(package):
+    import configparser
+    import requests
+
+    config = configparser.RawConfigParser()
+    config.read_file(open(fr'{package}/setup.cfg'))
+    local_version = config.get('metadata', 'version')
+    releases = requests.get(f"https://pypi.org/pypi/{package}/json").json()['releases']
+    shipped_versions = []
+    [shipped_versions.append(version) for version, details in releases.items()]
+
+    assert local_version not in shipped_versions

2ace05072566e074c4222485c741b98b8e37a95b
Jacob Salway <jacob.salway@gmail.com>
2023-01-29 03:30:02 +1100

    Add custom models directory argument (#105)
    Hey,
    
    I'm currently trying out the `DbtDag` and `DbtTaskGroup` classes for a
    project in the hope of replacing some bespoke `manifest.json` parsing
    code that we maintain, however I noticed the the dbt models directory is
    hard coded within the parser
    [here](https://github.com/astronomer/astronomer-cosmos/blob/e4abd7bbe043b46c7d66c18dca2d34d38cd03cc0/cosmos/providers/dbt/parser/project.py#L129).
    
    We use a custom models directory name and I'm sure others might too, so
    I thought I could contribute a small change upstream to enable a custom
    models directory to be specified.
    
    ---------
    
    Co-authored-by: Chris Hronek <31361051+pohek321@users.noreply.github.com>

--- cosmos/providers/dbt/task_group.py
+++ cosmos/providers/dbt/task_group.py
@@ -20,6 +20,7 @@ class DbtTaskGroup(CosmosTaskGroup):
 
     :param dbt_project_name: The name of the dbt project
     :param dbt_root_path: The path to the dbt root directory
+    :param dbt_models_dir: The path to the dbt models directory within the project
     :param conn_id: The Airflow connection ID to use for the dbt profile
     :param dbt_args: Parameters to pass to the underlying dbt operators
     :param emit_datasets: If enabled test nodes emit Airflow Datasets for downstream cross-DAG dependencies
@@ -36,6 +37,7 @@ class DbtTaskGroup(CosmosTaskGroup):
         dbt_args: Dict[str, Any] = {},
         emit_datasets: bool = True,
         dbt_root_path: str = "/usr/local/airflow/dbt",
+        dbt_models_dir: str = "models",
         test_behavior: Literal["none", "after_each", "after_all"] = "after_each",
         select: Dict[str, List[str]] = {},
         exclude: Dict[str, List[str]] = {},
@@ -52,6 +54,7 @@ class DbtTaskGroup(CosmosTaskGroup):
         group = render_project(
             dbt_project_name=dbt_project_name,
             dbt_root_path=dbt_root_path,
+            dbt_models_dir=dbt_models_dir,
             task_args=dbt_args,
             test_behavior=test_behavior,
             emit_datasets=emit_datasets,

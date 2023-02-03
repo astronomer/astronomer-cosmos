a5641d79abcb33ee27d66e38980849be9c6b1ab8
Chris Hronek <pohek321@gmail.com>
2023-02-02 17:17:31 -0700

    Forgot to add example dags

---
+++ examples/dags/jaffle_shop.py
@@ -0,0 +1,39 @@
+"""
+## Jaffle Shop DAG
+[Jaffle Shop](https://github.com/dbt-labs/jaffle_shop) is a fictional eCommerce store. This dbt project originates from
+dbt labs as an example project with dummy data to demonstrate a working dbt core project. This DAG uses the cosmos dbt
+parser to generate an Airflow TaskGroup from the dbt project folder
+
+"""
+from airflow import DAG
+from airflow.datasets import Dataset
+from airflow.operators.empty import EmptyOperator
+from pendulum import datetime
+
+from cosmos.providers.dbt.task_group import DbtTaskGroup
+
+with DAG(
+    dag_id="jaffle_shop",
+    start_date=datetime(2022, 11, 27),
+    schedule=[Dataset("SEED://JAFFLE_SHOP")],
+    doc_md=__doc__,
+    catchup=False,
+    default_args={"owner": "02-TRANSFORM"},
+) as dag:
+
+    pre_dbt_workflow = EmptyOperator(task_id="pre_dbt_workflow")
+
+    jaffle_shop = DbtTaskGroup(
+        dbt_project_name="jaffle_shop",
+        conn_id="airflow_db",
+        dbt_args={
+            "schema": "public",
+            "dbt_executable_path": "/usr/local/airflow/dbt_venv/bin/dbt",
+        },
+        test_behavior="after_all",
+        dag=dag,
+    )
+
+    post_dbt_workflow = EmptyOperator(task_id="post_dbt_workflow")
+
+    pre_dbt_workflow >> jaffle_shop >> post_dbt_workflow
---
+++ examples/dags/mrr-playbook.py
@@ -0,0 +1,29 @@
+"""
+## MRR Playbook DAG
+[MRR Playbook](https://github.com/dbt-labs/mrr-playbook) is a working dbt project demonstrating how to model
+subscription revenue. This dbt project originates from dbt labs as an example project with dummy data to demonstrate a
+working dbt core project. This DAG uses the cosmos dbt parser to generate a DAG from the dbt project folder
+
+"""
+
+from airflow.datasets import Dataset
+from pendulum import datetime
+
+from cosmos.providers.dbt.dag import DbtDag
+
+mrr_playbook = DbtDag(
+    dbt_project_name="mrr-playbook",
+    conn_id="airflow_db",
+    dbt_args={
+        "schema": "public",
+        "dbt_executable_path": "/usr/local/airflow/dbt_venv/bin/dbt",
+    },
+    dag_id="mrr_playbook",
+    start_date=datetime(2022, 11, 27),
+    schedule=[Dataset("SEED://MRR_PLAYBOOK")],
+    doc_md=__doc__,
+    catchup=False,
+    default_args={"owner": "02-TRANSFORM"},
+)
+
+mrr_playbook

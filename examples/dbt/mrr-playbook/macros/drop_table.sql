329811ce663cbc9fee65158b0c9a1da4d5f82cd1
Chris Hronek <pohek321@gmail.com>
2023-02-02 17:08:27 -0700

    Make a specific directory for astro_tests

--- examples/dbt/mrr-playbook/macros/drop_table.sql
+++ examples/dbt/mrr-playbook/macros/drop_table.sql
@@ -1,12 +1,6 @@
-{%- macro drop_table(table_name, conn_type) -%}
-    {% if conn_type != 'bigquery' and conn_type != 'databricks' %}
-      {%- set drop_query -%}
-          DROP TABLE IF EXISTS {{ target.schema }}.{{ table_name }} CASCADE
-      {%- endset -%}
-    {% else %}
-      {%- set drop_query -%}
-          DROP TABLE IF EXISTS {{ target.schema }}.{{ table_name }}
-      {%- endset -%}
-    {% endif %}
+{%- macro drop_table(table_name) -%}
+    {%- set drop_query -%}
+        DROP TABLE IF EXISTS {{ target.schema }}.{{ table_name }} CASCADE
+    {%- endset -%}
     {% do run_query(drop_query) %}
 {%- endmacro -%}
--- examples/dbt/mrr-playbook/models/customer_churn_month.sql
+++ examples/dbt/mrr-playbook/models/customer_churn_month.sql
@@ -7,32 +7,7 @@ with mrr as (
 -- row for month *after* last month of activity
 joined as (
 
-    {% if var('conn_type') == 'bigquery' %}
-      select
-          date_sub(date_month, interval 1 month) as date_month,
-          customer_id,
-          cast(0 as FLOAT64) as mrr,
-          false as is_active,
-          first_active_month,
-          last_active_month,
-          false as is_first_month,
-          false as is_last_month
-      from mrr
-      where is_last_month
-    {% elif var('conn_type') == 'databricks' %}
-      select
-        add_months(date_month, 1) as date_month,
-        customer_id,
-        0::float as mrr,
-        false as is_active,
-        first_active_month,
-        last_active_month,
-        false as is_first_month,
-        false as is_last_month
-    from mrr
-    where is_last_month
-    {% else %}
-      select
+    select
         date_month + interval '1 month' as date_month,
         customer_id,
         0::float as mrr,
@@ -41,11 +16,10 @@ joined as (
         last_active_month,
         false as is_first_month,
         false as is_last_month
+
     from mrr
+
     where is_last_month
-    {% endif %}
-
-
 
 )
 

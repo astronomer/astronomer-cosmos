22b52799ab8d34eeb72fafa403bd4eba4678d879
Daniel Imberman <danielryan2430@gmail.com>
2023-02-15 14:50:27 -0800

    Databricks/databricks workflow taskgroup (#108)
    This pull request creates a new Airflow task group called the
    DatabricksWorkflowTaskGroup. Databricks workflows are a popular new
    Databricks product that allows users to optimize their tasks for use on
    Databricks "job" clusters. The DatabricksWorkflowTaskGroup reads all
    tasks in the task group and submits them to Databricks as one job. Tasks
    will need to implement the "convert_to_databricks_workflow_task"
    function to work in a Databricks task group.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
 import pytest
+from airflow.models import DAG
+from airflow.utils.timezone import datetime
+
+TEST_DAG_ID = "unit_test_dag"
 
 
 def pytest_addoption(parser):
@@ -11,3 +15,12 @@ def package(request):
     if package_value is None:
         pytest.skip()
     return package_value
+
+
+@pytest.fixture
+def dag():
+    """
+    Creates a test DAG with default arguments.
+    """
+    dag = DAG(TEST_DAG_ID, start_date=datetime(2022, 1, 1))
+    yield dag

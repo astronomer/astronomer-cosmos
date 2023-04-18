9f53c95e417d7c972fccd37ffd4730e115cb0f9f
Julian LaNeve <lanevejulian@gmail.com>
2023-04-18 17:38:01 -0400

    new method of lazy loading

--- cosmos/providers/dbt/core/operators/docker.py
+++ cosmos/providers/dbt/core/operators/docker.py
@@ -8,10 +8,16 @@ from airflow.utils.context import Context
 
 from cosmos.providers.dbt.core.operators.base import DbtBaseOperator
 
-# docker is an optional dependency, so we use lazy loading to avoid import errors
-from .lazy_load import DockerOperator
-
 logger = logging.getLogger(__name__)
+
+# docker is an optional dependency, so we need to check if it's installed
+try:
+    from airflow.providers.docker.operators.docker import DockerOperator
+except ImportError:
+    raise ImportError(
+        "Could not import DockerOperator. Ensure you've installed the docker provider separately or "
+        "with with `pip install astronomer-cosmos[...,docker]`."
+    )
 
 
 class DbtDockerBaseOperator(DockerOperator, DbtBaseOperator):
--- cosmos/providers/dbt/core/operators/lazy_load.py
+++ cosmos/providers/dbt/core/operators/lazy_load.py
@@ -9,30 +9,3 @@ class MissingPackage:
             f" please make sure the right optional dependencies are installed."
             f" try - pip install astronomer-cosmos[...,{self.optional_dependency_name}]"
         )
-
-
-try:
-    from airflow.providers.docker.operators.docker import DockerOperator
-except ImportError:
-    DockerOperator = MissingPackage(
-        "airflow.providers.docker.operators.docker.DockerOperator", "docker"
-    )
-
-try:
-    from airflow.providers.cncf.kubernetes.backcompat.backwards_compat_converters import (
-        convert_env_vars,
-    )
-    from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-        KubernetesPodOperator,
-    )
-    from kubernetes.client import models as k8s
-except ImportError:
-    KubernetesPodOperator = MissingPackage(
-        "airflow.providers.cncf.kubernetes.operators.kubernetes_pod.KubernetesPodOperator",
-        "kubernetes",
-    )
-    convert_env_vars = MissingPackage(
-        "airflow.providers.cncf.kubernetes.backcompat.backwards_compat_converters.convert_env_vars",
-        "kubernetes",
-    )
-    k8s = MissingPackage("kubernetes.client.models", "kubernetes")

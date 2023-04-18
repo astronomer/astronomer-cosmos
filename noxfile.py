a4474cc57246701caa2624249d821f3725b83433
Julian LaNeve <lanevejulian@gmail.com>
2023-04-18 12:19:13 -0400

    add k8s and docker to noxfile

--- noxfile.py
+++ noxfile.py
@@ -42,7 +42,7 @@ def test(session: nox.Session, airflow) -> None:
     }
 
     session.install(f"apache-airflow=={airflow}")
-    session.install("-e", ".[all,tests]")
+    session.install("-e", ".[all,tests,docker,kubernetes]")
 
     # Log all the installed dependencies
     session.log("Installed Dependencies:")
@@ -67,7 +67,7 @@ def test(session: nox.Session, airflow) -> None:
 @nox.session(python=["3.8"])
 def type_check(session: nox.Session) -> None:
     """Run MyPy checks."""
-    session.install("-e", ".[all,tests]")
+    session.install("-e", ".[all,tests,docker,kubernetes]")
     session.run("mypy", "--version")
     session.run("mypy", "cosmos")
 

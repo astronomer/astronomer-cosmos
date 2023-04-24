35a36fa0fd8ec0b2a689f51420ca4c100dedba97
Julian LaNeve <lanevejulian@gmail.com>
2023-04-24 14:10:09 -0400

    remove hardcoded version

--- cosmos/tests/test_version.py
+++ cosmos/tests/test_version.py
@@ -7,13 +7,6 @@ import re
 import cosmos
 
 
-def test_version() -> None:
-    """
-    Test that the version is set correctly.
-    """
-    assert cosmos.__version__ == "0.6.2"
-
-
 def test_version_format() -> None:
     """
     Test that the version is set correctly.

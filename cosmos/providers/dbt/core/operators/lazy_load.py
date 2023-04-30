3ab0d557185177b7143c97e90cc53022d41e3d16
Julian LaNeve <lanevejulian@gmail.com>
2023-04-30 16:27:22 -0400

    fix import issues

--- cosmos/providers/dbt/core/operators/lazy_load.py
+++ cosmos/providers/dbt/core/operators/lazy_load.py
@@ -1,7 +1,9 @@
-class MissingPackage:
-    def __init__(self, module_name, optional_dependency_name):
+def MissingPackage(module_name, optional_dependency_name):
+    def raise_error(**kwargs):
         raise RuntimeError(
             f"Error loading the module {module_name},"
             f" please make sure the right optional dependencies are installed."
             f" try - pip install astronomer-cosmos[...,{optional_dependency_name}]"
         )
+
+    return raise_error

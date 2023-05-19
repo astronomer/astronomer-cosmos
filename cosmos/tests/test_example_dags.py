79bb91589ad030371c0209283952719b7ea3cf93
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-05-19 15:35:10 +0100

    Ignore DAGs inadequate based on Airflow version

--- cosmos/tests/test_example_dags.py
+++ cosmos/tests/test_example_dags.py
@@ -2,15 +2,28 @@ from __future__ import annotations
 
 from pathlib import Path
 
+import airflow
 import pytest
 from airflow.models.dagbag import DagBag
 from airflow.utils.db import create_default_connections
 from airflow.utils.session import provide_session
+from packaging.version import Version
 
 from . import utils as test_utils
 
 EXAMPLE_DAGS_DIR = Path(__file__).parent.parent.parent / "dev/dags"
 EXAMPLE_CONN_FILE = Path(__file__).parent / "test-connections.yaml"
+AIRFLOW_IGNORE_FILE = EXAMPLE_DAGS_DIR / ".airflowignore"
+
+MIN_VER_DAG_FILE: dict[str, list[str]] = {
+    "2.4": ["cosmos_seed_dag.py"],
+}
+
+# Sort descending based on Versions and convert string to an actual version
+MIN_VER_DAG_FILE_VER: dict[Version, list[str]] = {
+    Version(version): MIN_VER_DAG_FILE[version]
+    for version in sorted(MIN_VER_DAG_FILE, key=Version, reverse=True)
+}
 
 
 @provide_session
@@ -24,15 +37,23 @@ def session():
     return get_session()
 
 
-def get_dag_bag():
+def get_dag_bag() -> DagBag:
     """Create a DagBag by adding the files that are not supported to .airflowignore"""
-    dag_bag = DagBag(EXAMPLE_DAGS_DIR, include_examples=False)
-    assert dag_bag.dags
-    assert not dag_bag.import_errors
-    return dag_bag
+    with open(AIRFLOW_IGNORE_FILE, "w+") as file:
+        for min_version, files in MIN_VER_DAG_FILE_VER.items():
+            if Version(airflow.__version__) < min_version:
+                print(f"Adding {files} to .airflowignore")
+                file.writelines([f"{file}\n" for file in files])
+
+    print(".airflowignore contents: ")
+    print(AIRFLOW_IGNORE_FILE.read_text())
+    db = DagBag(EXAMPLE_DAGS_DIR, include_examples=False)
+    assert db.dags
+    assert not db.import_errors
+    return db
 
 
-def get_dag_ids():
+def get_dag_ids() -> list[str]:
     dag_bag = get_dag_bag()
     return dag_bag.dag_ids
 

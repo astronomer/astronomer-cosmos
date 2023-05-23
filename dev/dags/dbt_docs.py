572f4d713da952727fd5a2ef6525f391ed2250e1
szecsip <szecsip94@sch.bme.hu>
2023-05-23 22:01:16 +0000

    add modified dbt docs generate and upload example DAG

--- dev/dags/dbt_docs.py
+++ dev/dags/dbt_docs.py
@@ -14,56 +14,10 @@ from pathlib import Path
 from airflow import DAG
 from pendulum import datetime
 
-from cosmos.providers.dbt.core.operators import DbtDocsOperator
+from cosmos.providers.dbt.core.operators import DbtDocsS3Operator, DbtDocsAzureStorageOperator
 
 DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
-DEFAULT_DBT_DOCS_PATH = Path(__file__).parent / "dbt-docs"
 DBT_ROOT_PATH = os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH)
-DBT_DOCS_PATH = os.getenv("DBT_DOCS_PATH", DEFAULT_DBT_DOCS_PATH)
-AWS_CONN = "aws_default"
-
-
-def docs_callback(project_dir: str) -> None:
-    """
-    Callback function to print the path to the generated docs.
-    """
-    target_dir = f"{project_dir}/target"
-
-    try:
-        from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-
-        hook = S3Hook(aws_conn_id=AWS_CONN)
-
-        # iterate over the files in the target dir and upload them to S3
-        for dirpath, _, filenames in os.walk(target_dir):
-            for filename in filenames:
-                hook.load_file(
-                    filename=f"{dirpath}/{filename}",
-                    bucket_name="my-bucket",
-                    key=f"dbt-docs/{filename}",
-                    replace=True,
-                )
-
-        return
-
-    # if the S3Hook isn't installed, just copy the target dir to a local dir
-    except ImportError:
-        pass
-
-    # if there's a botocore.exceptions.NoCredentialsError, print a warning and just copy the docs locally
-    except Exception as exc:
-        if "NoCredentialsError" in str(exc):
-            print(
-                "WARNING: No AWS credentials found.\
-                To upload docs to S3, install the S3Hook and configure an S3 connection."
-            )
-
-    # copy the target dir to /usr/local/airflow/dbt-docs
-    if os.path.exists(DBT_DOCS_PATH):
-        shutil.rmtree(DBT_DOCS_PATH)
-
-    shutil.copytree(target_dir, DBT_DOCS_PATH)
-
 
 with DAG(
     dag_id="docs_dag",
@@ -72,12 +26,22 @@ with DAG(
     doc_md=__doc__,
     catchup=False,
 ) as dag:
-    generate_dbt_docs = DbtDocsOperator(
-        task_id="generate_dbt_docs",
+    generate_dbt_docs_aws = DbtDocsS3Operator(
+        task_id="generate_dbt_docs_aws",
         project_dir=DBT_ROOT_PATH,
-        schema="public",
         conn_id="airflow_db",
-        callback=docs_callback,
+        schema='public',
+        target_conn_id="test_aws",
+        bucket_name="test_bucket",
     )
 
-    generate_dbt_docs
+    generate_dbt_docs_azure = DbtDocsAzureStorageOperator(
+        task_id="generate_dbt_docs_azure",
+        project_dir=DBT_ROOT_PATH,
+        conn_id="airflow_db",
+        schema='public',
+        target_conn_id="test_azure",
+        container_name="$web",
+    )
+
+    [generate_dbt_docs_aws, generate_dbt_docs_azure]

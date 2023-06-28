f85a334ff8b9d068c32a9bda2f570e4fcf157a2b
JoeSham <j.samanek@seznam.cz>
2023-06-28 14:14:01 +0200

    Add support for BQ SA keyfile_dict
    Related issue: https://github.com/astronomer/astronomer-cosmos/issues/350

--- cosmos/profiles/bigquery/__init__.py
+++ cosmos/profiles/bigquery/__init__.py
@@ -1,5 +1,9 @@
 "BigQuery Airflow connection -> dbt profile mappings"
 
 from .service_account_file import GoogleCloudServiceAccountFileProfileMapping
+from .service_account_keyfile_dict import GoogleCloudServiceAccountDictProfileMapping
 
-__all__ = ["GoogleCloudServiceAccountFileProfileMapping"]
+__all__ = [
+    "GoogleCloudServiceAccountFileProfileMapping",
+    "GoogleCloudServiceAccountDictProfileMapping",
+]

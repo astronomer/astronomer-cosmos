5a22129eca90479adad1162eb229058884e504c6
Patrick Hess <phess.ph@gmail.com>
2023-07-19 11:53:35 -0700

    finalize private key tests

--- tests/profiles/snowflake/test_snowflake_user_privatekey.py
+++ tests/profiles/snowflake/test_snowflake_user_privatekey.py
@@ -157,7 +157,7 @@ def test_profile_env_vars(
         mock_snowflake_conn.conn_id,
     )
     assert profile_mapping.env_vars == {
-        "COSMOS_CONN_SNOWFLAKE_PRIVATE_KEY_CONTENT": mock_snowflake_conn.password,
+        "COSMOS_CONN_SNOWFLAKE_PRIVATE_KEY_CONTENT": mock_snowflake_conn.extra_dejson.get("private_key_content"),
     }
 
 
@@ -169,13 +169,13 @@ def test_old_snowflake_format() -> None:
         conn_id="my_snowflake_connection",
         conn_type="snowflake",
         login="my_user",
-        password="my_password",
         schema="my_schema",
         extra=json.dumps(
             {
                 "extra__snowflake__account": "my_account",
                 "extra__snowflake__database": "my_database",
                 "extra__snowflake__warehouse": "my_warehouse",
+                "extra__snowflake__private_key_content": "my_private_key",
             }
         ),
     )
@@ -184,7 +184,7 @@ def test_old_snowflake_format() -> None:
     assert profile_mapping.profile == {
         "type": conn.conn_type,
         "user": conn.login,
-        "password": "{{ env_var('COSMOS_CONN_SNOWFLAKE_PRIVATE_KEY_CONTENT') }}",
+        "private_key_content": "{{ env_var('COSMOS_CONN_SNOWFLAKE_PRIVATE_KEY_CONTENT') }}",
         "schema": conn.schema,
         "account": conn.extra_dejson.get("account"),
         "database": conn.extra_dejson.get("database"),
@@ -200,7 +200,6 @@ def test_appends_region() -> None:
         conn_id="my_snowflake_connection",
         conn_type="snowflake",
         login="my_user",
-        password="my_password",
         schema="my_schema",
         extra=json.dumps(
             {
@@ -208,6 +207,7 @@ def test_appends_region() -> None:
                 "region": "my_region",
                 "database": "my_database",
                 "warehouse": "my_warehouse",
+                "private_key_content": "my_private_key",
             }
         ),
     )
@@ -216,7 +216,7 @@ def test_appends_region() -> None:
     assert profile_mapping.profile == {
         "type": conn.conn_type,
         "user": conn.login,
-        "password": "{{ env_var('COSMOS_CONN_SNOWFLAKE_PASSWORD') }}",
+        "private_key_content": "{{ env_var('COSMOS_CONN_SNOWFLAKE_PRIVATE_KEY_CONTENT') }}",
         "schema": conn.schema,
         "account": f"{conn.extra_dejson.get('account')}.{conn.extra_dejson.get('region')}",
         "database": conn.extra_dejson.get("database"),

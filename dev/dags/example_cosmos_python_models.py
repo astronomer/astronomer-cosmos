99b9446cb1052c8cb7e632b7988dff904335b234
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-07-19 11:39:32 +0100

    Copy original jaffle shop example using:
    cp dev/dags/basic_cosmos_dag.py dev/dags/example_cosmos_python_models.py
    cp -r dev/dags/dbt/jaffle_shop  dev/dags/dbt/jaffle_shop_python

---
+++ dev/dags/example_cosmos_python_models.py
@@ -0,0 +1,31 @@
+"""
+An example DAG that uses Cosmos to render a dbt project.
+"""
+
+import os
+from datetime import datetime
+from pathlib import Path
+
+from cosmos import DbtDag
+
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+
+# [START local_example]
+basic_cosmos_dag = DbtDag(
+    # dbt/cosmos-specific parameters
+    dbt_root_path=DBT_ROOT_PATH,
+    dbt_project_name="jaffle_shop",
+    conn_id="airflow_db",
+    profile_args={
+        "schema": "public",
+    },
+    profile_name_override="airflow",
+    target_name_override="dev_target",
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="basic_cosmos_dag",
+)
+# [END local_example]

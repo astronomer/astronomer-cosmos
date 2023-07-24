2979a17677fd3c58245c4b5141d6f98d701fa5ff
Julian LaNeve <lanevejulian@gmail.com>
2023-07-24 00:44:31 -0400

    add better support for passing down args

--- cosmos/core/airflow.py
+++ cosmos/core/airflow.py
@@ -1,9 +1,9 @@
 import importlib
 import logging
+import inspect
 
 from airflow.models import BaseOperator
 from airflow.models.dag import DAG
-from airflow.utils.task_group import TaskGroup
 
 from cosmos.core.graph.entities import Task
 
@@ -23,14 +23,25 @@ def get_airflow_task(task: Task, dag: DAG, task_group: "TaskGroup | None" = None
     # fully qualified name defined in the task
     module_name, class_name = task.operator_class.rsplit(".", 1)
     module = importlib.import_module(module_name)
-    Operator = getattr(module, class_name)
+    operator = getattr(module, class_name)
 
-    airflow_task = Operator(
-        task_id=task.id,
-        dag=dag,
-        task_group=task_group,
+    # ensure we only pass the arguments that the operator expects
+    supported_args = set()
+    for inherited_class in operator.mro():
+        for arg in inspect.signature(inherited_class.__init__).parameters:
+            supported_args.add(arg)
+
+    potential_operator_args = {
+        "task_id": task.id,
+        "dag": dag,
+        "task_group": task_group,
         **task.arguments,
-    )
+    }
+    operator_args = {
+        arg_key: arg_value for arg_key, arg_value in potential_operator_args.items() if arg_key in supported_args
+    }
+
+    airflow_task = operator(**operator_args)
 
     if not isinstance(airflow_task, BaseOperator):
         raise TypeError(f"Operator class {task.operator_class} is not a subclass of BaseOperator")

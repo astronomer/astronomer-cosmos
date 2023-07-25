8dda4475c4a521d70ab4fcb8552fca9d479732af
Harel Shein <harel.shein@gmail.com>
2023-07-25 14:44:04 -0400

    tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,11 +12,11 @@ PIPELINE_FOLDER = "jaffle_shop"
 
 # Tests that a ProjectConfig object can be created with valid parameters
 def test_valid_parameters():
-    project_config = ProjectConfig(dbt_project="path/to/dbt/project")
+    project_config = ProjectConfig(dbt_project_path="path/to/dbt/project")
     assert project_config.dbt_project_path == Path("path/to/dbt/project")
-    assert project_config.models_path == Path("path/to/dbt/project/models")
-    assert project_config.seeds_path == Path("path/to/dbt/project/seeds")
-    assert project_config.snapshots_path == Path("path/to/dbt/project/snapshots")
+    assert project_config.models_relative_path == Path("path/to/dbt/project/models")
+    assert project_config.seeds_relative_path == Path("path/to/dbt/project/seeds")
+    assert project_config.snapshots_relative_path == Path("path/to/dbt/project/snapshots")
     assert project_config.manifest_path is None
 
 

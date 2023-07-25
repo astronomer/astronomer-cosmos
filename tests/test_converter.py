d2dabde59577fe6af63b526060f8aaef11c6fb30
Harel Shein <harel.shein@gmail.com>
2023-07-24 22:02:34 -0400

    match cosmos exception type

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,5 +1,5 @@
 import pytest
-from airflow.exceptions import AirflowException
+from cosmos.exceptions import CosmosValueError
 
 
 from cosmos.converter import validate_arguments
@@ -12,7 +12,7 @@ def test_validate_arguments_tags(argument_key):
     exclude = [f"{selector_name}:b,{selector_name}:c"]
     profile_args = {}
     task_args = {}
-    with pytest.raises(AirflowException) as err:
+    with pytest.raises(CosmosValueError) as err:
         validate_arguments(select, exclude, profile_args, task_args)
     expected = f"Can't specify the same {selector_name} in `select` and `exclude`: {{'b'}}"
     assert err.value.args[0] == expected

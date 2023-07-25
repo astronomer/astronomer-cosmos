4cf1fe94a6048d44cd164a44276b3efa78710e68
Harel Shein <harel.shein@gmail.com>
2023-07-24 22:09:28 -0400

    self ref

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,9 +2,8 @@ from pathlib import Path
 
 from cosmos.config import ProjectConfig
 
-
 # Tests that a ProjectConfig object can be created with valid parameters
-def test_valid_parameters(self):
+def test_valid_parameters():
     project_config = ProjectConfig(dbt_project="path/to/dbt/project")
     assert project_config.dbt_project_path == Path("path/to/dbt/project")
     assert project_config.models_path == Path("path/to/dbt/project/models")

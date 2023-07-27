0774497a8d2ebaeb6f4b61178d694c04f2608e9b
Julian LaNeve <lanevejulian@gmail.com>
2023-07-27 11:55:43 -0400

    only log env var keys

--- cosmos/operators/local.py
+++ cosmos/operators/local.py
@@ -169,7 +169,7 @@ class DbtLocalBaseOperator(DbtBaseOperator):
                 ]
 
                 logger.info("Trying to run the command:\n %s\nFrom %s", full_cmd, tmp_project_dir)
-                logger.info("Using environment variables:\n %s", env)
+                logger.info("Using environment variables keys: %s", env.keys())
                 result = self.run_subprocess(
                     command=full_cmd,
                     env=env,

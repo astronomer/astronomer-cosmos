4de2d51da1480ced2b7fb4cf9e4364b4f1ba23a8
DanMawdsleyBA <dan.mawdsley@ba.com>
2023-08-22 14:51:52 +0100

    add test for dbt deps

--- tests/dbt/test_graph.py
+++ tests/dbt/test_graph.py
@@ -252,6 +252,28 @@ def test_load_via_dbt_ls_with_invalid_dbt_path():
     assert err_info.value.args[0] == expected
 
 
+def test_load_via_dbt_ls_without_dbt_deps():
+    dbt_project = DbtProject(name="jaffle_shop", root_dir=DBT_PROJECTS_ROOT_DIR)
+    dbt_graph = DbtGraph(
+        dbt_deps=False,
+        project=dbt_project,
+        profile_config=ProfileConfig(
+            profile_name="default",
+            target_name="default",
+            profile_mapping=PostgresUserPasswordProfileMapping(
+                conn_id="airflow_db",
+                profile_args={"schema": "public"},
+            ),
+        ),
+    )
+
+    with pytest.raises(CosmosLoadDbtException) as err_info:
+        dbt_graph.load_via_dbt_ls()
+
+    expected = "Unable to run dbt ls command due to dbt_packages not installed."
+    assert err_info.value.args[0] == expected
+
+
 @pytest.mark.integration
 @patch("cosmos.dbt.graph.Popen.communicate", return_value=("Some Runtime Error", ""))
 def test_load_via_dbt_ls_with_runtime_error_in_stdout(mock_popen_communicate):

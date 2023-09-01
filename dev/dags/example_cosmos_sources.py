bd05c169de589068efd6f5a8dfc1e5b98c9a7cb4
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-09-01 16:51:02 +0100

    Improve example DAG

--- dev/dags/example_cosmos_sources.py
+++ dev/dags/example_cosmos_sources.py
@@ -3,25 +3,19 @@ from datetime import datetime
 from pathlib import Path
 
 from airflow.operators.dummy import DummyOperator
+from airflow.models.dag import DAG
+from airflow.utils.task_group import TaskGroup
 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig
 from cosmos.constants import DbtResourceType
+from cosmos.dbt.graph import DbtNode
 
 DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
 DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
 
 os.environ["DBT_SQLITE_PATH"] = str(DEFAULT_DBT_ROOT_PATH / "simple")
 
-"""
-profile_config = ProfileConfig(
-    profile_name="default",
-    target_name="dev",
-    profile_mapping=PostgresUserPasswordProfileMapping(
-        conn_id="airflow_db",
-        profile_args={"schema": "public"},
-    ),
-)
-"""
+
 profile_config = ProfileConfig(
     profile_name="simple",
     target_name="dev",
@@ -29,13 +23,13 @@ profile_config = ProfileConfig(
 )
 
 
-def convert_source(dag, task_group, node, **kwargs):
+def convert_source(dag: DAG, task_group: TaskGroup, node: DbtNode, **kwargs):
     return DummyOperator(dag=dag, task_group=task_group, task_id=f"{node.name}_source")
 
 
 render_config = RenderConfig(dbt_resource_converter={DbtResourceType.SOURCE: convert_source})
 
-# [START local_example]
+
 example_cosmos_sources = DbtDag(
     # dbt/cosmos-specific parameters
     project_config=ProjectConfig(
@@ -50,4 +44,3 @@ example_cosmos_sources = DbtDag(
     catchup=False,
     dag_id="example_cosmos_sources",
 )
-# [END local_example]

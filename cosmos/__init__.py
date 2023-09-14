4b49eb3dff88e3176e4af04bb5e0931b11727911
Julian LaNeve <lanevejulian@gmail.com>
2023-09-14 13:38:38 -0400

    bump version

--- cosmos/__init__.py
+++ cosmos/__init__.py
@@ -5,7 +5,7 @@ Astronomer Cosmos is a library for rendering dbt workflows in Airflow.
 
 Contains dags, task groups, and operators.
 """
-__version__ = "1.1.1"
+__version__ = "1.1.1a1"
 
 from cosmos.airflow.dag import DbtDag
 from cosmos.airflow.task_group import DbtTaskGroup

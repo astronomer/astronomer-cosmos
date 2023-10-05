709f92fb2f3b6e898f3e78bc02eb6a9f58461f57
Julian LaNeve <lanevejulian@gmail.com>
2023-10-05 13:42:24 -0400

    Ensure filtering with manifest loading works with single model

---
+++ dev/dags/cosmos_manifest_example.py
@@ -0,0 +1,40 @@
+"""
+An example DAG that uses Cosmos to render a dbt project.
+"""
+
+import os
+from datetime import datetime
+from pathlib import Path
+
+from cosmos import DbtDag, ProjectConfig, ProfileConfig, RenderConfig, LoadMode
+from cosmos.profiles import PostgresUserPasswordProfileMapping
+
+DEFAULT_DBT_ROOT_PATH = Path(__file__).parent / "dbt"
+DBT_ROOT_PATH = Path(os.getenv("DBT_ROOT_PATH", DEFAULT_DBT_ROOT_PATH))
+
+profile_config = ProfileConfig(
+    profile_name="default",
+    target_name="dev",
+    profile_mapping=PostgresUserPasswordProfileMapping(
+        conn_id="airflow_db",
+        profile_args={"schema": "public"},
+    ),
+)
+
+# [START local_example]
+cosmos_manifest_example = DbtDag(
+    # dbt/cosmos-specific parameters
+    project_config=ProjectConfig(
+        DBT_ROOT_PATH / "jaffle_shop",
+        manifest_path=DBT_ROOT_PATH / "jaffle_shop" / "target" / "manifest.json",
+    ),
+    profile_config=profile_config,
+    render_config=RenderConfig(load_method=LoadMode.DBT_MANIFEST, select=["path:models/customers.sql"]),
+    operator_args={"install_deps": True},
+    # normal dag parameters
+    schedule_interval="@daily",
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    dag_id="cosmos_manifest_example",
+)
+# [END local_example]

e47514659b4f5158ce8e26fd5c1d0e51739771e8
David Spulak <david.spulak@maginteractive.se>
2023-10-17 13:50:44 +0200

    Update test - tags and config.tags to be identical (usually the case) - add consistency with has_child and is_child tag for non-test nodes

--- tests/dbt/test_selector.py
+++ tests/dbt/test_selector.py
@@ -45,7 +45,7 @@ grandparent_node = DbtNode(
     depends_on=[],
     file_path=SAMPLE_PROJ_PATH / "gen1/models/grandparent.sql",
     tags=["has_child"],
-    config={"materialized": "view"},
+    config={"materialized": "view", "tags": ["has_child"]},
 )
 parent_node = DbtNode(
     name="parent",
@@ -53,8 +53,8 @@ parent_node = DbtNode(
     resource_type=DbtResourceType.MODEL,
     depends_on=["grandparent"],
     file_path=SAMPLE_PROJ_PATH / "gen2/models/parent.sql",
-    tags=["has_child"],
-    config={"materialized": "view"},
+    tags=["has_child", "is_child"],
+    config={"materialized": "view", "tags": ["has_child", "is_child"]},
 )
 child_node = DbtNode(
     name="child",
@@ -62,8 +62,8 @@ child_node = DbtNode(
     resource_type=DbtResourceType.MODEL,
     depends_on=["parent"],
     file_path=SAMPLE_PROJ_PATH / "gen3/models/child.sql",
-    tags=["nightly"],
-    config={"materialized": "table", "tags": ["is_child"]},
+    tags=["nightly", "is_child"],
+    config={"materialized": "table", "tags": ["nightly", "is_child"]},
 )
 
 grandchild_1_test_node = DbtNode(
@@ -72,8 +72,8 @@ grandchild_1_test_node = DbtNode(
     resource_type=DbtResourceType.MODEL,
     depends_on=["parent"],
     file_path=SAMPLE_PROJ_PATH / "gen3/models/grandchild_1.sql",
-    tags=["nightly"],
-    config={"materialized": "table", "tags": ["deprecated", "test"]},
+    tags=["nightly", "deprecated", "test"],
+    config={"materialized": "table", "tags": ["nightly", "deprecated", "test"]},
 )
 
 grandchild_2_test_node = DbtNode(
@@ -82,8 +82,8 @@ grandchild_2_test_node = DbtNode(
     resource_type=DbtResourceType.MODEL,
     depends_on=["parent"],
     file_path=SAMPLE_PROJ_PATH / "gen3/models/grandchild_2.sql",
-    tags=["nightly"],
-    config={"materialized": "table", "tags": ["deprecated", "test2"]},
+    tags=["nightly", "deprecated", "test2"],
+    config={"materialized": "table", "tags": ["nightly", "deprecated", "test2"]},
 )
 
 sample_nodes = {

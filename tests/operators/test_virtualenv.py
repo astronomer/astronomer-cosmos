a2e40220d337ebfe4a80ac56ff3cf86e647e4233
Lennart Kloppenburg <lennartkloppenburg@live.nl>
2023-10-18 15:58:00 +0200

    Add basic test

--- tests/operators/test_virtualenv.py
+++ tests/operators/test_virtualenv.py
@@ -74,10 +74,36 @@ def test_run_command(
     assert dbt_cmd["command"][1] == "do-something"
     assert mock_execute.call_count == 2
 
+<<<<<<< HEAD
 
 def test_virtualenv_operator_append_env_is_true_by_default():
     venv_operator = ConcreteDbtVirtualenvBaseOperator(
         dag=DAG("sample_dag", start_date=datetime(2024, 4, 16)),
+=======
+@patch("airflow.utils.python_virtualenv.execute_in_subprocess")
+@patch("cosmos.operators.virtualenv.DbtLocalBaseOperator.calculate_openlineage_events_completes")
+@patch("cosmos.operators.virtualenv.DbtLocalBaseOperator.store_compiled_sql")
+@patch("cosmos.operators.virtualenv.DbtLocalBaseOperator.exception_handling")
+@patch("cosmos.operators.virtualenv.DbtLocalBaseOperator.subprocess_hook")
+@patch("airflow.hooks.base.BaseHook.get_connection")
+def test_supply_virtualenv_dir_flag(    
+    mock_get_connection,
+    mock_subprocess_hook,
+    mock_exception_handling,
+    mock_store_compiled_sql,
+    mock_calculate_openlineage_events_completes,
+    mock_execute,
+):
+    mock_get_connection.return_value = Connection(
+        conn_id="fake_conn",
+        conn_type="postgres",
+        host="fake_host",
+        port=5432,
+        login="fake_login",
+        password="fake_password",
+        schema="fake_schema",
+    )
+    venv_operator = DbtVirtualenvBaseOperator(
         profile_config=profile_config,
         task_id="fake_task",
         install_deps=True,
@@ -87,6 +113,8 @@ def test_virtualenv_operator_append_env_is_true_by_default():
         py_requirements=["dbt-postgres==1.6.0b1"],
         emit_datasets=False,
         invocation_mode=InvocationMode.SUBPROCESS,
+        virtualenv_dir = "mock-venv",
     )
 
     assert venv_operator.append_env is True
+    assert venv_operator.venv_dbt_path == "mock-venv/bin/dbt"

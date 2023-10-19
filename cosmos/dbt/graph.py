31c1d4ac334329cf3ca96c440d63b1ef2920b41b
edgarasnavickas <edgaras.navickas@kevin.eu>
2023-10-19 16:32:18 +0300

    resolving issues with the DBT_MANIFEST load method when has_test attribute is not assigned to the node correctly

--- cosmos/dbt/graph.py
+++ cosmos/dbt/graph.py
@@ -330,9 +330,6 @@ class DbtGraph:
         However, since the Manifest does not represent filters, it relies on the Custom Cosmos implementation
         to filter out the nodes relevant to the user (based on self.exclude and self.select).
 
-        Noted that if dbt project contains versioned models, need to use dbt>=1.6.0 instead. Because, as dbt<1.6.0,
-        dbt cli doesn't support select a specific versioned models as stg_customers_v1, customers_v1, ...
-
         Updates in-place:
         * self.nodes
         * self.filtered_nodes
@@ -341,6 +338,7 @@ class DbtGraph:
         nodes = {}
         with open(self.project.manifest_path) as fp:  # type: ignore[arg-type]
             manifest = json.load(fp)
+            node_dependencies = manifest.get("child_map", {})
 
             resources = {**manifest.get("nodes", {}), **manifest.get("sources", {}), **manifest.get("exposures", {})}
             for unique_id, node_dict in resources.items():
@@ -361,20 +359,28 @@ class DbtGraph:
                 project_dir=self.project.dir, nodes=nodes, select=self.select, exclude=self.exclude
             )
 
-            self.update_node_dependency()
+            self.update_node_dependency(node_dependencies=node_dependencies)
 
         logger.info("Total nodes: %i", len(self.nodes))
         logger.info("Total filtered nodes: %i", len(self.nodes))
 
-    def update_node_dependency(self) -> None:
+    def update_node_dependency(self, node_dependencies={}) -> None:
         """
-        This will update the property `has_text` if node has `dbt` test
+        This will update the property `has_test` if node has `dbt` test
 
         Updates in-place:
         * self.filtered_nodes
         """
-        for _, node in self.filtered_nodes.items():
-            if node.resource_type == DbtResourceType.TEST:
-                for node_id in node.depends_on:
-                    if node_id in self.filtered_nodes:
-                        self.filtered_nodes[node_id].has_test = True
+        if node_dependencies:
+            for node_id, node in self.filtered_nodes.items():
+                if node.resource_type == DbtResourceType.MODEL:
+                    for dependency in node_dependencies[node_id]:
+                        if self.nodes[dependency].resource_type == DbtResourceType.TEST:
+                            self.filtered_nodes[node_id].has_test = True
+                            break
+        else:
+            for _, node in self.filtered_nodes.items():
+                if node.resource_type == DbtResourceType.TEST:
+                    for node_id in node.depends_on:
+                        if node_id in self.filtered_nodes:
+                            self.filtered_nodes[node_id].has_test = True

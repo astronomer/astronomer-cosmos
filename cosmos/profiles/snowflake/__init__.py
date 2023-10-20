b7381c8ebf40f7bb31c8010133ba6b1f243ce460
ivanstillfront <105354508+ivanstillfront@users.noreply.github.com>
2023-10-20 04:14:24 -0700

    Add ProfileMapping for Snowflake encrypted private key path (#608)
    Add support for Snowflake Airflow Connections with encrypted
    private key paths via `extra.private_key_file` and `password`
    configuration.
    
    Closes: #607

--- cosmos/profiles/snowflake/__init__.py
+++ cosmos/profiles/snowflake/__init__.py
@@ -2,5 +2,10 @@
 
 from .user_pass import SnowflakeUserPasswordProfileMapping
 from .user_privatekey import SnowflakePrivateKeyPemProfileMapping
+from .user_encrypted_privatekey import SnowflakeEncryptedPrivateKeyPemProfileMapping
 
-__all__ = ["SnowflakeUserPasswordProfileMapping", "SnowflakePrivateKeyPemProfileMapping"]
+__all__ = [
+    "SnowflakeUserPasswordProfileMapping",
+    "SnowflakePrivateKeyPemProfileMapping",
+    "SnowflakeEncryptedPrivateKeyPemProfileMapping",
+]

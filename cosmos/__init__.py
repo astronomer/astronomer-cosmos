f57d03de4816bc0cbbddf9345776d66348353df5
Andrew Greenburg <agreenburg@vergeventures.net>
2023-11-03 17:28:52 -0400

    Add cosmos/propagate_logs Airflow config support for disabling log propagation if desired

--- cosmos/__init__.py
+++ cosmos/__init__.py
@@ -116,3 +116,28 @@ __all__ = [
     "LoadMode",
     "TestBehavior",
 ]
+
+"""
+Required provider info for using Airflow config for configuration
+"""
+def get_provider_info():
+    return {
+        "package-name": "astronomer-cosmos",  # Required
+        "name": "Astronomer Cosmos",  # Required
+        "description": "Astronomer Cosmos is a library for rendering dbt workflows in Airflow. Contains dags, task groups, and operators.",  # Required
+        "versions": [__version__],  # Required
+        "config": {
+            "cosmos": {
+                "description": None,
+                "options": {
+                    "propagate_logs": {
+                        "description": "Enable log propagation from Cosmos custom logger\n",
+                        "version_added": "1.3.0a1",
+                        "type": "boolean",
+                        "example": None,
+                        "default": "True",
+                    },
+                },
+            },
+        },
+    }
--- tests/test_log.py
+++ tests/test_log.py
@@ -1,6 +1,7 @@
 import logging
 
 from cosmos.log import get_logger
+from airflow.configuration import conf
 
 
 def test_get_logger():
@@ -12,3 +13,10 @@ def test_get_logger():
     assert custom_logger.propagate is True
     assert custom_logger.handlers[0].formatter.__class__.__name__ == "CustomTTYColoredFormatter"
     assert custom_string in custom_logger.handlers[0].formatter._fmt
+
+def test_propagate_logs_conf():
+    if not conf.has_section("cosmos"):
+        conf.add_section("cosmos")
+    conf.set("cosmos","propagate_logs","False")
+    custom_logger = get_logger("cosmos-log")
+    assert custom_logger.propagate is False

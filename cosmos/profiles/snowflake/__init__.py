d9c26316a9bd7fb2cc3e4d03eb1dfac60d4a8b88
DanMawdsleyBA <dan.mawdsley@ba.com>
2023-11-04 10:56:05 +0000

    Intial change for Snowflake encrypted private key

--- cosmos/profiles/snowflake/__init__.py
+++ cosmos/profiles/snowflake/__init__.py
@@ -4,6 +4,8 @@ from .user_encrypted_privatekey_env_variable import SnowflakeEncryptedPrivateKey
 from .user_encrypted_privatekey_file import SnowflakeEncryptedPrivateKeyFilePemProfileMapping
 from .user_pass import SnowflakeUserPasswordProfileMapping
 from .user_privatekey import SnowflakePrivateKeyPemProfileMapping
+from .user_encrypted_privatekey_file import SnowflakeEncryptedPrivateKeyFilePemProfileMapping
+from .user_encrypted_privatekey_env_variable import SnowflakeEncryptedPrivateKeyPemProfileMapping
 
 __all__ = [
     "SnowflakeUserPasswordProfileMapping",

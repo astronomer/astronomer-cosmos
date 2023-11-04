feaae3e3f4384a47157bbf1b2de586a0da0ec9f1
DanMawdsleyBA <dan.mawdsley@ba.com>
2023-11-04 11:26:10 +0000

    Adding conditions on claiming connections

--- cosmos/profiles/snowflake/user_encrypted_privatekey_env_variable.py
+++ cosmos/profiles/snowflake/user_encrypted_privatekey_env_variable.py
@@ -45,6 +45,16 @@ class SnowflakeEncryptedPrivateKeyPemProfileMapping(BaseProfileMapping):
         "private_key_passphrase": "password",
     }
 
+    def can_claim_connection(self) -> bool:
+        # Make sure this isn't a private key path credential
+        result = super().can_claim_connection()
+        if (
+            result
+            and self.conn.extra_dejson.get("private_key_file") is not None
+        ):
+            return False
+        return result
+
     @property
     def conn(self) -> Connection:
         """

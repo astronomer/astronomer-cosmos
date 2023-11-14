astronomer/astronomer-cosmos

commit 9cd46d2353dff874cc94c66baa0b146255ec42f5

Add `operator_args` `full_refresh` as a templated field (#623)
This allows you to fully refresh a model from the console.
Full-refresh/backfill is a common task. Using Airflow parameters makes
this easy. Without this, you'd have to trigger an entire deployment. In our
setup, company analysts manage their models without modifying
the DAG code. This empowers such users.

Example of usage:
```python
with DAG(
        dag_id="jaffle",
        params={"full_refresh": Param(default=False, type="boolean")},
        render_template_as_native_obj=True
):
    task = DbtTaskGroup(
        operator_args={"full_refresh": "{{ params.get('full_refresh') }}", "install_deps": True},
    )

```

Closes: #151

cosmos/operators/local.py
@@ -397,6 +397,8 @@ class DbtSeedLocalOperator(DbtLocalBaseOperator):
 
     ui_color = "#F58D7E"
 
+    template_fields: Sequence[str] = DbtBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
+
     def __init__(self, full_refresh: bool = False, **kwargs: Any) -> None:
         self.full_refresh = full_refresh
         super().__init__(**kwargs)
@@ -434,6 +436,7 @@ class DbtRunLocalOperator(DbtLocalBaseOperator):
 
     ui_color = "#7352BA"
     ui_fgcolor = "#F4F2FC"
+    template_fields: Sequence[str] = DbtBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
 
     def __init__(self, full_refresh: bool = False, **kwargs: Any) -> None:
         self.full_refresh = full_refresh

tests/operators/test_local.py
@@ -420,6 +420,19 @@ def test_calculate_openlineage_events_completes_openlineage_errors(mock_processo
     assert err_msg in caplog.text
 
 
+@pytest.mark.parametrize(
+    "operator_class,expected_template",
+    [
+        (DbtSeedLocalOperator, ("env", "vars", "full_refresh")),
+        (DbtRunLocalOperator, ("env", "vars", "full_refresh")),
+    ],
+)
+def test_dbt_base_operator_template_fields(operator_class, expected_template):
+    # Check if value of template fields is what we expect for the operators we're validating
+    dbt_base_operator = operator_class(profile_config=profile_config, task_id="my-task", project_dir="my/dir")
+    assert dbt_base_operator.template_fields == expected_template
+
+
 @patch.object(DbtDocsGCSLocalOperator, "required_files", ["file1", "file2"])
 def test_dbt_docs_gcs_local_operator():
     mock_gcs = MagicMock()

commit ee91ece155433adac8b9ac9531aa11d7235acc03

Store compiled_sql even when task fails (fixes issue #369) (#671)
Update `DbtLocalBaseOperator` code to store `compiled_sql` prior to
exception handling so that when a task fails, the `compiled_sql` can still
be reviewed.

In the process found and fixed a related bug where `compiled_sql` was
being dropped on some operations due to the way that the `full_refresh`
field was being added to the `template_fields`.

Closes #369

Fixes bug introduced in
https://github.com/astronomer/astronomer-cosmos/pull/623 where
compiled_sql was being lost in `DbtSeedLocalOperator` and
`DbtRunLocalOperator`

Co-authored-by: Andrew Greenburg <agreenburg@vergeventures.net>

cosmos/operators/local.py
@@ -247,8 +247,8 @@ class DbtLocalBaseOperator(DbtBaseOperator):
                     logger.info("Outlets: %s", outlets)
                     self.register_dataset(inlets, outlets)
 
-                self.exception_handling(result)
                 self.store_compiled_sql(tmp_project_dir, context)
+                self.exception_handling(result)
                 if self.callback:
                     self.callback(tmp_project_dir)
 
@@ -398,7 +398,7 @@ class DbtSeedLocalOperator(DbtLocalBaseOperator):
 
     ui_color = "#F58D7E"
 
-    template_fields: Sequence[str] = DbtBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
+    template_fields: Sequence[str] = DbtLocalBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
 
     def __init__(self, full_refresh: bool = False, **kwargs: Any) -> None:
         self.full_refresh = full_refresh
@@ -437,7 +437,7 @@ class DbtRunLocalOperator(DbtLocalBaseOperator):
 
     ui_color = "#7352BA"
     ui_fgcolor = "#F4F2FC"
-    template_fields: Sequence[str] = DbtBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
+    template_fields: Sequence[str] = DbtLocalBaseOperator.template_fields + ("full_refresh",)  # type: ignore[operator]
 
     def __init__(self, full_refresh: bool = False, **kwargs: Any) -> None:
         self.full_refresh = full_refresh

tests/operators/test_local.py
@@ -423,8 +423,8 @@ def test_calculate_openlineage_events_completes_openlineage_errors(mock_processo
 @pytest.mark.parametrize(
     "operator_class,expected_template",
     [
-        (DbtSeedLocalOperator, ("env", "vars", "full_refresh")),
-        (DbtRunLocalOperator, ("env", "vars", "full_refresh")),
+        (DbtSeedLocalOperator, ("env", "vars", "compiled_sql", "full_refresh")),
+        (DbtRunLocalOperator, ("env", "vars", "compiled_sql", "full_refresh")),
     ],
 )
 def test_dbt_base_operator_template_fields(operator_class, expected_template):
99f4146eb2555299bc755db19c3fbb8ebb05b0ba
DanMawdsleyBA <dan.mawdsley@ba.com>
2023-11-30 13:21:41 +0000

    Adding dbt_packages when dbt_deps is true

--- cosmos/dbt/project.py
+++ cosmos/dbt/project.py
@@ -6,13 +6,17 @@ from cosmos.constants import (
     DBT_LOG_DIR_NAME,
     DBT_TARGET_DIR_NAME,
 )
+from cosmos.config import RenderConfig
 from contextlib import contextmanager
 from typing import Generator
 
 
 def create_symlinks(project_path: Path, tmp_dir: Path) -> None:
     """Helper function to create symlinks to the dbt project files."""
-    ignore_paths = (DBT_LOG_DIR_NAME, DBT_TARGET_DIR_NAME, "dbt_packages", "profiles.yml")
+    if RenderConfig.dbt_deps:
+        ignore_paths = (DBT_LOG_DIR_NAME, DBT_TARGET_DIR_NAME, "profiles.yml")
+    else:
+        ignore_paths = (DBT_LOG_DIR_NAME, DBT_TARGET_DIR_NAME, "dbt_packages", "profiles.yml")
     for child_name in os.listdir(project_path):
         if child_name not in ignore_paths:
             os.symlink(project_path / child_name, tmp_dir / child_name)

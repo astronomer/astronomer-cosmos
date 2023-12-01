d0851d7b8f6d4fde38d5eecb84b7d1710460d7bf
Justin Bandoro <justin.bandoro@kevala.com>
2023-12-01 06:43:30 -0800

    use cache instead of lru_cache for readability

--- tests/test_example_dags.py
+++ tests/test_example_dags.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
-from functools import lru_cache
+from functools import cache
 
 import airflow
 import pytest
@@ -38,7 +38,7 @@ def session():
     return get_session()
 
 
-@lru_cache
+@cache
 def get_dag_bag() -> DagBag:
     """Create a DagBag by adding the files that are not supported to .airflowignore"""
     with open(AIRFLOW_IGNORE_FILE, "w+") as file:

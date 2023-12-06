53f78e08a7d8bfdd56e106f0bab583f567f65622
adammarples <adammarples@gmail.com>
2023-12-06 15:27:40 +0000

    Revert "Revert "fix selector logic for test nodes""
    This reverts commit e9a0e0ab3ccb34f1bb5396c5048c63b18d1ddae8.

--- cosmos/dbt/selector.py
+++ cosmos/dbt/selector.py
@@ -295,7 +295,13 @@ class NodeSelector:
         self.visited_nodes.add(node_id)
 
         if node.resource_type == DbtResourceType.TEST:
-            node.tags = getattr(self.nodes.get(node.depends_on[0]), "tags", [])
+            dependency_ids = [node_id for node_id in node.depends_on if node_id.startswith('model.')]
+            parent_id = dependency_ids[-1]
+            if len(dependency_ids) > 1:
+                logger.warning(
+                    f'Test node {node.name} has more than one model dependency {dependency_ids}, selected tags from parent assumed to be {parent_id}.')
+            parent_tags = getattr(self.nodes.get(parent_id), "tags", [])
+            node.tags = list(set(node.tags + parent_tags))
 
         if not self._is_tags_subset(node):
             return False

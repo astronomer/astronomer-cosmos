285a6a99c583c15f053a1b9348668c80a3d0441e
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-01-25 11:56:32 +0000

    Fix custom selector when `test` node has no `depends_on` values (#814)
    A user reported the following issue while using Cosmos 1.3.1:
    
    ```
        def _should_include_node(self, node_id: str, node: DbtNode) -> bool:
            "Checks if a single node should be included. Only runs once per node with caching."
            if node_id in self.visited_nodes:
                return node_id in self.selected_nodes
    
            self.visited_nodes.add(node_id)
    
            if node.resource_type == DbtResourceType.TEST:
    >           node.tags = getattr(self.nodes.get(node.depends_on[0]), "tags", [])
    E           IndexError: list index out of range
    
    cosmos/dbt/selector.py:298: IndexError
    ```
    
    In order to reproduce this issue, it was necessary to add a tag-based
    select statement.
    
    Based on the error, it seems their dbt project has a test without
    `depends_on`. This CR adds support to this use case.
    
    Closes: #813

--- cosmos/dbt/selector.py
+++ cosmos/dbt/selector.py
@@ -295,7 +295,7 @@ class NodeSelector:
 
         self.visited_nodes.add(node_id)
 
-        if node.resource_type == DbtResourceType.TEST:
+        if node.resource_type == DbtResourceType.TEST and node.depends_on:
             node.tags = getattr(self.nodes.get(node.depends_on[0]), "tags", [])
             logger.debug(
                 "The test node <%s> inherited these tags from the parent node <%s>: %s",

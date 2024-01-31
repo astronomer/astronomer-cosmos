c9f8e239c16ae06f09c22b88915eef680835a97a
Julian LaNeve <lanevejulian@gmail.com>
2024-01-31 18:27:20 -0500

    fix typo

--- scripts/test/unit-cov.sh
+++ scripts/test/unit-cov.sh
@@ -6,4 +6,4 @@ pytest \
     --durations=0 \
     -m "not integration" \
     --ignore=tests/test_example_dags.py \
-    --ignore=tests/test_example_dags_no_connections.p
+    --ignore=tests/test_example_dags_no_connections.py

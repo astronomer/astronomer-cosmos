140fa77a20ad628b1bf2d8464f0db1c5c8fdcc32
Julian LaNeve <lanevejulian@gmail.com>
2024-01-31 19:58:13 -0500

    ignore perf tests in unit tests

--- scripts/test/unit-cov.sh
+++ scripts/test/unit-cov.sh
@@ -4,6 +4,7 @@ pytest \
     --cov-report=term-missing \
     --cov-report=xml \
     --durations=0 \
-    -m "not integration" \
+    -m "not (integration or perf)" \
+    --ignore=tests/perf \
     --ignore=tests/test_example_dags.py \
     --ignore=tests/test_example_dags_no_connections.py

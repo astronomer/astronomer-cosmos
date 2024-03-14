222d052662684997ff6528ad6ae0ef60cf8b3a3a
AlgirdasDubickas <123624084+AlgirdasDubickas@users.noreply.github.com>
2024-03-14 15:45:36 +0200

    fix:Adjust dbt_packages handling based on packages.yml existence
    his commit refines the logic for handling the `dbt_packages` directory during the symlink creation process in `create_symlinks` function. Previously, the decision to ignore `dbt_packages` was solely based on the `ignore_dbt_packages` flag. The updated logic now also checks for the presence of a `packages.yml` file within the project directory.
    
    - If `ignore_dbt_packages` is True and `packages.yml` exists, the function will add `dbt_packages` to the list of directories to ignore. This ensures that the symlink creation process respects the project's dependency management setup.

--- cosmos/dbt/project.py
+++ cosmos/dbt/project.py
@@ -12,9 +12,14 @@ from cosmos.constants import DBT_LOG_DIR_NAME, DBT_PARTIAL_PARSE_FILE_NAME, DBT_
 def create_symlinks(project_path: Path, tmp_dir: Path, ignore_dbt_packages: bool) -> None:
     """Helper function to create symlinks to the dbt project files."""
     ignore_paths = [DBT_LOG_DIR_NAME, DBT_TARGET_DIR_NAME, "profiles.yml"]
+
     if ignore_dbt_packages:
-        # this is linked to dbt deps so if dbt deps is true then ignore existing dbt_packages folder
-        ignore_paths.append("dbt_packages")
+        # Check for the presence of 'packages.yml' in the project directory:
+        if (project_path / 'packages.yml').exists():
+            # This is linked to dbt deps so if dbt deps is true then ignore existing dbt_packages folder
+            ignore_paths.append("dbt_packages")
+
+    
     for child_name in os.listdir(project_path):
         if child_name not in ignore_paths:
             os.symlink(project_path / child_name, tmp_dir / child_name)

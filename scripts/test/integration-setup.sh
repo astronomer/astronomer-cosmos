d5e512e1ecd89e4e5243de72646172584b847f7a
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-05-09 00:45:11 +0100

    Fix CI issue (Airflow 2.9 being installed when 2.5 should have been installed)

--- scripts/test/integration-setup.sh
+++ scripts/test/integration-setup.sh
@@ -1,6 +1,6 @@
 # we install using the following workaround to overcome installation conflicts, such as:
 # apache-airflow 2.3.0 and dbt-core [0.13.0 - 1.5.2] and jinja2>=3.0.0 because these package versions have conflicting dependencies
 pip uninstall -y dbt-postgres dbt-databricks dbt-vertica;
-pip install 'dbt-core' 'dbt-databricks' 'dbt-postgres' 'dbt-vertica' 'openlineage-airflow' 'apache-airflow-providers-postgres';
+pip install 'dbt-core' 'dbt-databricks' 'dbt-postgres' 'dbt-vertica' 'openlineage-airflow'
 rm -rf airflow.*;
 airflow db init;

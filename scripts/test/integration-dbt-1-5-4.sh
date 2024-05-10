1837803f8fac4adceb97a1439987debde01d43f4
Pankaj Koti <pankaj.koti@astronomer.io>
2024-05-10 22:28:52 +0530

    Correct stale `root_path` in partial parse file (#950)
    With the introduction of enabling partial parse in PR #904, 
    upon testing the implementation, it is observed that the seeds 
    files were not been able to be located as the partial parse file 
    contained a stale `root_path` from previous command runs. 
    This issue is observed on specific earlier versions of dbt-core like
    `1.5.4` and `1.6.5`, but not on recent versions of dbt-core `1.5.8`,
    `1.6.6`
    and `1.7.0`. I am suspecting that PR
    https://github.com/dbt-labs/dbt-core/pull/8762
    is likely the fix and the fix appears to be backported to later version 
    releases of `1.5.x` and `1.6.x`.
    
    However, irrespective of the dbt-core version, this PR attempts to 
    correct the `root_path` in the partial parse file by replacing it with 
    the needed project directory where the project files are located. 
    And thus ensures that the feature runs correctly for older and 
    newer versions of dbt-core.
    
    closes: #937
    
    ---------
    
    Co-authored-by: Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>

---
+++ scripts/test/integration-dbt-1-5-4.sh
@@ -0,0 +1,12 @@
+pip uninstall dbt-adapters dbt-common dbt-core dbt-extractor dbt-postgres dbt-semantic-interfaces -y
+pip install dbt-postgres==1.5.4  dbt-databricks==1.5.4
+rm -rf airflow.*; \
+airflow db init; \
+pytest -vv \
+    --cov=cosmos \
+    --cov-report=term-missing \
+    --cov-report=xml \
+    --durations=0 \
+    -m integration  \
+    --ignore=tests/perf \
+    -k 'basic_cosmos_task_group'

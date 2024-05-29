32c47cadda80747fafeaa9b0a36d92f043f03681
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-05-29 09:56:20 +0100

    Fix CI issues (#1005)
    Fix three problems observed while running the tests in the CI recently:
    
    1. Static check
    
    Fix spelling issue captured by static check (#1000)
    
    2. Integration test that's no longer needed
    
    The issue we were trying to capture for no longer happens in the latest
    version of the Apache Airflow provider
    `apache-airflow-providers-postgres==5.11.1rc1`:
    https://github.com/apache/airflow/issues/39842
    
    3. Skip a buggy version of OL
    
    There was a breaking change between
    `openlineage-integration-common==1.14.0` and
    `openlineage-integration-common==1.15.0` . It may have been an
    unintended side-effect of
    https://github.com/OpenLineage/OpenLineage/pull/2693.
    
    This is an example of how Cosmos was using `DbtLocalArtifactProcessor` -
    something that had been agreed upon in the past:
    
    ```
          openlineage_processor = DbtLocalArtifactProcessor(
                producer=OPENLINEAGE_PRODUCER,
                job_namespace=LINEAGE_NAMESPACE,
                project_dir=project_dir,
                profile_name=self.profile_config.profile_name,
                target=self.profile_config.target_name,
            )
            events = openlineage_processor.parse()
            for completed in events.completes:
                for output in getattr(completed, source):
                    dataset_uri = output.namespace + "/" + output.name
                    uris.append(dataset_uri)
    ```
    
    In `openlineage-integration-common==1.14.0` and earlier versions, this
    would create URIs in the format:
    ```
    postgres://0.0.0.0:5432/postgres.public.stg_customers
    ```
    Since openlineage-integration-common==1.15.0 , this leads to URIs being
    created in the format:
    ```
    postgres.public.stg_customers/postgres://0.0.0.0:5432
    ```
    
    This was fixed in https://github.com/OpenLineage/OpenLineage/pull/2735
    and released as part of OL 1.16:
    https://github.com/OpenLineage/OpenLineage/releases/tag/1.16.0

--- tests/operators/test_local.py
+++ tests/operators/test_local.py
@@ -23,7 +23,6 @@ from cosmos.dbt.parser.output import (
     parse_number_of_warnings_dbt_runner,
     parse_number_of_warnings_subprocess,
 )
-from cosmos.exceptions import AirflowCompatibilityError
 from cosmos.operators.local import (
     DbtBuildLocalOperator,
     DbtDocsAzureStorageLocalOperator,
@@ -441,48 +440,6 @@ def test_run_operator_dataset_inlets_and_outlets(caplog):
     assert run_operator.outlets == [Dataset(uri="postgres://0.0.0.0:5432/postgres.public.stg_customers", extra=None)]
     assert test_operator.inlets == [Dataset(uri="postgres://0.0.0.0:5432/postgres.public.stg_customers", extra=None)]
     assert test_operator.outlets == []
-
-
-@pytest.mark.skipif(
-    version.parse(airflow_version) not in PARTIALLY_SUPPORTED_AIRFLOW_VERSIONS,
-    reason="Airflow 2.9.0 and 2.9.1 have a breaking change in Dataset URIs",
-    # https://github.com/apache/airflow/issues/39486
-)
-@pytest.mark.integration
-def test_run_operator_dataset_emission_fails(caplog):
-
-    with DAG("test-id-1", start_date=datetime(2022, 1, 1)) as dag:
-        seed_operator = DbtSeedLocalOperator(
-            profile_config=real_profile_config,
-            project_dir=DBT_PROJ_DIR,
-            task_id="seed",
-            dbt_cmd_flags=["--select", "raw_customers"],
-            install_deps=True,
-            append_env=True,
-        )
-        run_operator = DbtRunLocalOperator(
-            profile_config=real_profile_config,
-            project_dir=DBT_PROJ_DIR,
-            task_id="run",
-            dbt_cmd_flags=["--models", "stg_customers"],
-            install_deps=True,
-            append_env=True,
-        )
-
-        seed_operator >> run_operator
-
-    with pytest.raises(AirflowCompatibilityError) as exc:
-        run_test_dag(dag)
-
-    err_msg = str(exc.value)
-    assert (
-        "Apache Airflow 2.9.0 & 2.9.1 introduced a breaking change in Dataset URIs, to be fixed in newer versions"
-        in err_msg
-    )
-    assert (
-        "If you want to use Cosmos with one of these Airflow versions, you will have to disable emission of Datasets"
-        in err_msg
-    )
 
 
 @pytest.mark.skipif(

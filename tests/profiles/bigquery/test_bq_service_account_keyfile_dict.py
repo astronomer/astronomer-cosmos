b364e39e83c8bbe7ff2de223adbd114f927fbafb
Pankaj <pankaj.singh@astronomer.io>
2024-06-05 18:56:50 +0530

    Add test

--- tests/profiles/bigquery/test_bq_service_account_keyfile_dict.py
+++ tests/profiles/bigquery/test_bq_service_account_keyfile_dict.py
@@ -65,7 +65,7 @@ def test_connection_claiming_succeeds(mock_bigquery_conn_with_dict: Connection):
 
 def test_connection_claiming_fails(mock_bigquery_conn_with_dict: Connection):
     # Remove the `project` key, which is mandatory
-    mock_bigquery_conn_with_dict.extra = json.dumps({"keyfile_dict": sample_keyfile_dict})
+    mock_bigquery_conn_with_dict.extra = json.dumps({"dataset": "my_dataset", "keyfile_dict": sample_keyfile_dict})
     profile_mapping = GoogleCloudServiceAccountDictProfileMapping(mock_bigquery_conn_with_dict, {})
     assert not profile_mapping.can_claim_connection()
 

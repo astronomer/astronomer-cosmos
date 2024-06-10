32a37890c916a4010942ba365a285298cdea434d
dwreeves <xdanielreeves@gmail.com>
2024-06-09 22:07:14 -0400

    got it

--- cosmos/plugin/__init__.py
+++ cosmos/plugin/__init__.py
@@ -139,8 +139,25 @@ iframe_script = """
       )
     }
   }
+
+  // Prevent parent hash changes from sending a message back to the parent.
+  // This is necessary for making sure the browser back button works properly.
+  let hashChangeLock = true;
+
   window.addEventListener('hashchange', function () {
-    window.parent.postMessage(window.location.hash, '*');
+    if (!hashChangeLock) {
+      window.parent.postMessage(window.location.hash);
+    }
+    hashChangeLock = false;
+  });
+  window.addEventListener('message', function (event) {
+    let msgData = event.data;
+    if (typeof msgData === 'string' && msgData.startsWith('#!')) {
+      let updateUrl = new URL(window.location);
+      updateUrl.hash = msgData;
+      hashChangeLock = true;
+      history.replaceState(null, null, updateUrl);
+    }
   });
 </script>
 """

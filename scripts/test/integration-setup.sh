dde28101dc6e5f157228719ad565898c8cf21418
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-06-18 17:23:25 +0100

    troubleshoot

--- scripts/test/integration-setup.sh
+++ scripts/test/integration-setup.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
+
+set -x
+
 # we install using the following workaround to overcome installation conflicts, such as:
 # apache-airflow 2.3.0 and dbt-core [0.13.0 - 1.5.2] and jinja2>=3.0.0 because these package versions have conflicting dependencies
-pip uninstall -y dbt-postgres dbt-databricks dbt-vertica; \
-rm -rf airflow.*; \
-airflow db init; \
+pip uninstall -y dbt-postgres dbt-databricks dbt-vertica
+rm -rf airflow.*
+pip freeze | grep airflow
+airflow db migrate
 pip install 'dbt-core' 'dbt-databricks' 'dbt-postgres' 'dbt-vertica' 'openlineage-airflow'

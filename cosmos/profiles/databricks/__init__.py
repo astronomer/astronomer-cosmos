a471817bd6461b9c2a4cedb07e852e40e624f924
Giovanni Silva <corsetti@ufpr.br>
2024-07-13 20:24:16 +0200

    fix CI

--- cosmos/profiles/databricks/__init__.py
+++ cosmos/profiles/databricks/__init__.py
@@ -1,5 +1,6 @@
 """Databricks Airflow connection -> dbt profile mappings"""
 
 from .token import DatabricksTokenProfileMapping
+from .client import DatabricksOauthProfileMapping
 
 __all__ = ["DatabricksTokenProfileMapping", "DatabricksOauthProfileMapping"]

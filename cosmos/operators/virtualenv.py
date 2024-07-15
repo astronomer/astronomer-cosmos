490855a4c3a8df3dd665472025e702421c03626f
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-07-15 11:16:44 +0100

    Add comments to improve the coverage

--- cosmos/operators/virtualenv.py
+++ cosmos/operators/virtualenv.py
@@ -36,6 +36,7 @@ PY_INTERPRETER = "python3"
 def depends_on_virtualenv_dir(method: Callable[[Any], Any]) -> Callable[[Any], Any]:
     def wrapper(operator: DbtVirtualenvBaseOperator, *args: Any) -> None:
         if operator.virtualenv_dir is None:
+            # TODO: test
             raise CosmosValueError(f"Method relies on value of parameter `virtualenv_dir` which is None.")
 
         method(operator, *args)
@@ -118,6 +119,7 @@ class DbtVirtualenvBaseOperator(DbtLocalBaseOperator):
         Delete the virtualenv directory if it is temporary.
         """
         if self.is_virtualenv_dir_temporary and self.virtualenv_dir and self.virtualenv_dir.exists():
+            # TODO: test
             self.log.info(f"Deleting the Python virtualenv {self.virtualenv_dir}")
             shutil.rmtree(str(self.virtualenv_dir), ignore_errors=True)
 
@@ -132,6 +134,7 @@ class DbtVirtualenvBaseOperator(DbtLocalBaseOperator):
             self.clean_dir_if_temporary()
 
     def on_kill(self) -> None:
+        # TODO: test
         self.clean_dir_if_temporary()
 
     def prepare_virtualenv(self) -> str:
@@ -160,6 +163,7 @@ class DbtVirtualenvBaseOperator(DbtLocalBaseOperator):
         # Use a reusable virtualenv
         self.log.info(f"Checking if the virtualenv lock {str(self.__lock_file)} exists")
         while not self._is_lock_available():
+            # TODO: test
             self.log.info("Waiting for virtualenv lock to be released")
             time.sleep(1)
 
@@ -191,9 +195,11 @@ class DbtVirtualenvBaseOperator(DbtLocalBaseOperator):
                 try:
                     _process_running = psutil.Process(pid).is_running()
                 except psutil.NoSuchProcess:
+                    # TODO: test
                     _process_running = False
 
                 self.log.info(f"Process {pid} running: {_process_running}")
+                # TODO: test
                 return not _process_running
 
         return True
@@ -214,11 +220,14 @@ class DbtVirtualenvBaseOperator(DbtLocalBaseOperator):
             return
 
         with open(self.__lock_file) as lf:
+            # TODO: test
             lock_file_pid = int(lf.read())
 
             if lock_file_pid == self._pid:
                 return self.__lock_file.unlink()
 
+            # TODO: test
+            # TODO: release lock if other process is not running
             self.log.warn(f"Lockfile owned by process of pid {lock_file_pid}, while operator has pid {self._pid}")
 
 

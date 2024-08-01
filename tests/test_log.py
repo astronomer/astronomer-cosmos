ced016818167dc7f3bf94f1e886ded373ba1bbcd
Daniel Reeves <xdanielreeves@gmail.com>
2024-07-31 23:56:42 -0400

    update

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,38 +2,37 @@ import logging
 
 import pytest
 
+import cosmos.log
 from cosmos import get_provider_info
 from cosmos.log import get_logger
+from cosmos.log import CosmosRichLogger
 
 
-def test_get_logger():
-    custom_string = "%(purple)s(astronomer-cosmos)%(reset)s"
-    standard_logger = logging.getLogger()
-    assert custom_string not in standard_logger.handlers[0].formatter._fmt
+def test_get_logger(monkeypatch):
+    monkeypatch.setattr(cosmos.log, "rich_logging", False)
+    standard_logger = get_logger("cosmos")
+    assert not isinstance(standard_logger, CosmosRichLogger)
 
-    custom_logger = get_logger("cosmos-log")
-    assert custom_logger.propagate is True
-    assert custom_logger.handlers[0].formatter.__class__.__name__ == "CustomTTYColoredFormatter"
-    assert custom_string in custom_logger.handlers[0].formatter._fmt
+    monkeypatch.setattr(cosmos.log, "rich_logging", True)
+    custom_logger = get_logger("cosmos")
+    assert isinstance(custom_logger, CosmosRichLogger)
 
     with pytest.raises(TypeError):
         # Ensure that the get_logger signature is not changed in the future
         # and name is still a required parameter
-        custom_logger = get_logger()  # noqa
-
-    # Explicitly ensure that even if we pass None or empty string
-    # we will not get root logger in any case
-    custom_logger = get_logger("")
-    assert custom_logger.name != ""
-
-    custom_logger = get_logger(None)  # noqa
-    assert custom_logger.name != ""
+        bad_logger = get_logger()  # noqa
 
 
-def test_propagate_logs_conf(monkeypatch):
-    monkeypatch.setattr("cosmos.log.propagate_logs", False)
-    custom_logger = get_logger("cosmos-log")
-    assert custom_logger.propagate is False
+def test_rich_logging(monkeypatch, capsys):
+    monkeypatch.setattr(cosmos.log, "rich_logging", False)
+    standard_logger = get_logger("cosmos")
+    standard_logger.info("Hello, world!")
+    assert capsys.readouterr() == "Hello, world!\n"
+
+    monkeypatch.setattr(cosmos.log, "rich_logging", True)
+    custom_logger = get_logger("cosmos")
+    custom_logger.info("Hello, world!")
+    assert capsys.readouterr() == "\x1b[35m(astronomer-cosmos)\x1b[0m Hello, world!\n"
 
 
 def test_get_provider_info():

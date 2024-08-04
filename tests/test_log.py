0252d4620bb1b8687d2c2502b4f914b9c6fa559c
Daniel Reeves <xdanielreeves@gmail.com>
2024-08-04 17:17:42 -0400

    test fixes

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,20 +1,17 @@
-import logging
-
 import pytest
 
 import cosmos.log
 from cosmos import get_provider_info
-from cosmos.log import get_logger
-from cosmos.log import CosmosRichLogger
+from cosmos.log import CosmosRichLogger, get_logger
 
 
 def test_get_logger(monkeypatch):
     monkeypatch.setattr(cosmos.log, "rich_logging", False)
-    standard_logger = get_logger("cosmos")
+    standard_logger = get_logger("test-get-logger-example1")
     assert not isinstance(standard_logger, CosmosRichLogger)
 
     monkeypatch.setattr(cosmos.log, "rich_logging", True)
-    custom_logger = get_logger("cosmos")
+    custom_logger = get_logger("test-get-logger-example2")
     assert isinstance(custom_logger, CosmosRichLogger)
 
     with pytest.raises(TypeError):
@@ -25,14 +22,20 @@ def test_get_logger(monkeypatch):
 
 def test_rich_logging(monkeypatch, capsys):
     monkeypatch.setattr(cosmos.log, "rich_logging", False)
-    standard_logger = get_logger("cosmos")
+    standard_logger = get_logger("test-rich-logging-example1")
     standard_logger.info("Hello, world!")
-    assert capsys.readouterr() == "Hello, world!\n"
+    out = capsys.readouterr().out
+    assert "Hello, world!" in out
+    assert "\x1b[35m(astronomer-cosmos)\x1b[0m " not in out
+    assert out.count("\n") == 1
 
     monkeypatch.setattr(cosmos.log, "rich_logging", True)
-    custom_logger = get_logger("cosmos")
+    custom_logger = get_logger("test-rich-logging-example2")
     custom_logger.info("Hello, world!")
-    assert capsys.readouterr() == "\x1b[35m(astronomer-cosmos)\x1b[0m Hello, world!\n"
+    out = capsys.readouterr().out
+    assert "Hello, world!" in out
+    assert "\x1b[35m(astronomer-cosmos)\x1b[0m " in out
+    assert out.count("\n") == 1
 
 
 def test_get_provider_info():

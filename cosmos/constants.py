8a860edf46fc7b31e037814c166d4440f2761587
Agata <65040128+ags-de@users.noreply.github.com>
2024-09-26 08:51:46 +0200

    Add GCP_CLOUD_RUN_JOB execution mode (#1153)
    Added new `GCP_CLOUD_RUN_JOB` execution mode that triggers Google Cloud
    Platform's Cloud Run Job instance with dbt model in it.
    
    It extends Airflow's `CloudRunExecuteJobOperator` and overrides Cloud
    Run Job's container with dbt command generated by cosmos.
    
    Note: `CloudRunExecuteJobOperator` has `container_overrides` parameter
    implemented in `apache-airflow-providers-google==10.13.0` which is
    supported by `airflow >=2.6.0`.
    
    Resolves #1149 
    
    Co-authored-by: Agata Zalewska <azalewska5@grupazpr.pl>
    Co-authored-by: Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>

--- cosmos/constants.py
+++ cosmos/constants.py
@@ -91,6 +91,7 @@ class ExecutionMode(Enum):
     AWS_EKS = "aws_eks"
     VIRTUALENV = "virtualenv"
     AZURE_CONTAINER_INSTANCE = "azure_container_instance"
+    GCP_CLOUD_RUN_JOB = "gcp_cloud_run_job"
 
 
 class InvocationMode(Enum):

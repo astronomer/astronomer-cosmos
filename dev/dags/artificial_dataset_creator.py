e252996165a114c66c79929aff962524f66d39b3
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-10-03 11:37:02 +0100

    Aritficial DAG that creates Dataset/DatasetAlias similar to Cosmos

---
+++ dev/dags/artificial_dataset_creator.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from airflow import DAG, Dataset
+from airflow.datasets import DatasetAlias
+from airflow.models.baseoperator import BaseOperator
+from airflow.utils.context import Context
+
+
+ALIAS_NAME = "basic_cosmos_dag__orders__run"
+DATASET_URI = 'postgres://0.0.0.0:5434/postgres.public.orders'
+
+
+class CustomOperator(BaseOperator):
+
+    def __init__(self, *args, **kwargs):
+        kwargs["outlets"] = [DatasetAlias(name=ALIAS_NAME)]
+        super().__init__(*args, **kwargs)
+
+    def execute(self, context: Context):
+        new_outlets = [Dataset(DATASET_URI)]
+        for outlet in new_outlets:
+            context["outlet_events"][ALIAS_NAME].add(outlet)
+
+
+with DAG("artificial_dataset_creator", start_date=datetime(2023, 4, 20)) as dag:
+    do_something = CustomOperator(task_id="do_something")
+    do_something

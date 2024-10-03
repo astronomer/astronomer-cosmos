0b23f911f9ed3474d3966bae6a4ccf1fe9fa4c9c
pankajastro <pankaj.singh@astronomer.io>
2024-10-03 13:06:35 +0530

    Bump dbt adaptor version for sqlite test

--- scripts/test/integration-sqlite-setup.sh
+++ scripts/test/integration-sqlite-setup.sh
@@ -1,4 +1,4 @@
 pip uninstall -y dbt-core dbt-sqlite openlineage-airflow openlineage-integration-common; \
 rm -rf airflow.*; \
 airflow db init; \
-pip install 'dbt-core==1.4' 'dbt-sqlite<=1.4' 'dbt-databricks<=1.4' 'dbt-postgres<=1.4'
+pip install 'dbt-core==1.5' 'dbt-sqlite<=1.5' 'dbt-databricks<=1.5' 'dbt-postgres<=1.5' 'dbt-bigquery<=1.5'

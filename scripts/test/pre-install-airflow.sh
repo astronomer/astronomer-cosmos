6d2688755d455122cf5fbd9aabac8a99210d9c96
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-11-01 11:58:14 +0000

    Fix k8s deps

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -31,10 +31,10 @@ uv pip -v install "apache-airflow-providers-google" --constraint /tmp/constraint
 uv pip -v install "apache-airflow-providers-microsoft-azure" --constraint /tmp/constraint.txt
 uv pip -v install "apache-airflow-providers-postgres" --constraint /tmp/constraint.txt
 
-if [ "$AIRFLOW_VERSION" >= "2.7" ] ; then
-  uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt
-else
+if [ "$AIRFLOW_VERSION" == "2.4" || "$AIRFLOW_VERSION" == "2.5" || "$AIRFLOW_VERSION" == "2.6" ] ; then
    uv pip install "apache-airflow-providers-kubernetes" --constraint /tmp/constraint.txt
+else
+  uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt
 fi
 
 rm /tmp/constraint.txt

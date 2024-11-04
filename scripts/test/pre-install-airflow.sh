829f8f940cc3c0c9ed862a86f4b893b358312003
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-11-04 12:10:32 +0000

    Fix CI https://github.com/astronomer/astronomer-cosmos/actions/runs/11631397136/job/32392401417

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -21,20 +21,22 @@ curl -sSL $CONSTRAINT_URL -o /tmp/constraint.txt
 # Workaround to remove PyYAML constraint that will work on both Linux and MacOS
 sed '/PyYAML==/d' /tmp/constraint.txt > /tmp/constraint.txt.tmp
 mv /tmp/constraint.txt.tmp /tmp/constraint.txt
+
 # Install Airflow with constraints
 pip install uv
+
 uv pip install "apache-airflow==$AIRFLOW_VERSION" --constraint /tmp/constraint.txt
-uv pip install pydantic --constraint /tmp/constraint.txt
-uv pip -v install "apache-airflow-providers-amazon[s3fs]" --constraint /tmp/constraint.txt
-uv pip -v install "apache-airflow-providers-docker" --constraint /tmp/constraint.txt
-uv pip -v install "apache-airflow-providers-google" --constraint /tmp/constraint.txt
-uv pip -v install "apache-airflow-providers-microsoft-azure" --constraint /tmp/constraint.txt
-uv pip -v install "apache-airflow-providers-postgres" --constraint /tmp/constraint.txt
-uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt
+uv pip install apache-airflow-providers-docker --constraint /tmp/constraint.txt
+uv pip install apache-airflow-providers-google --constraint /tmp/constraint.txt
+uv pip install apache-airflow-providers-microsoft-azure --constraint /tmp/constraint.txt
+uv pip install apache-airflow-providers-postgres --constraint /tmp/constraint.txt
 
 if [ "$AIRFLOW_VERSION" = "2.4" ]; then
-  uv pip -v install "apache-airflow-providers-amazon[s3fs]>=3.0.0"
+  uv pip install "apache-airflow-providers-amazon[s3fs]~=8.7.1"
   uv pip install "apache-airflow-providers-cncf-kubernetes>=5.1.1"
+else
+  uv pip install "apache-airflow-providers-amazon[s3fs]" --constraint /tmp/constraint.txt
+  uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt
 fi
 
 rm /tmp/constraint.txt

c80d53f45541f171240b27c171eb25644822f884
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2024-11-04 13:34:31 +0000

    Fix py2.6 tests issue

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -42,11 +42,11 @@ elif [ "$AIRFLOW_VERSION" = "2.5" ] ; then
   uv pip install "apache-airflow-providers-google~=10.12.0"
   uv pip install "apache-airflow-providers-microsoft-azure~=8.3.0"
 elif [ "$AIRFLOW_VERSION" = "2.6" ] ; then
-  uv pip install "typing_extensions<4.6"
   uv pip install "apache-airflow-providers-amazon[s3fs]~=8.20.0"
   uv pip install "apache-airflow-providers-cncf-kubernetes~=8.1.1"
   uv pip install "apache-airflow-providers-google~=10.17.0"
   uv pip install "apache-airflow-providers-microsoft-azure~=9.0.1"
+  uv pip install "typing_extensions<4.6"
 else
   uv pip install "apache-airflow-providers-amazon[s3fs]>=3.0.0" # --constraint /tmp/constraint.txt
   uv pip install "apache-airflow-providers-cncf-kubernetes>=5.1.1" # --constraint /tmp/constraint.txt

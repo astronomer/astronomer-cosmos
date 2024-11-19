98ac03bebd71f23672539b9874a678cac11fadf4
Pankaj Koti <pankajkoti699@gmail.com>
2024-11-19 15:18:28 +0530

    Add comment on why not use constraints for google-provider

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -44,6 +44,11 @@ elif [ "$AIRFLOW_VERSION" = "2.7" ] ; then
 else
   uv pip install "apache-airflow-providers-amazon[s3fs]" --constraint /tmp/constraint.txt
   uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt
+  # The Airflow 2.9 constraints file at https://raw.githubusercontent.com/apache/airflow/constraints-2.9.0/constraints-3.11.txt
+  # specifies apache-airflow-providers-google==10.16.0. However, our CI setup uses a Google connection without a token,
+  # which previously led to authentication issues when the token was None. This issue was resolved in PR #38102 and
+  # fixed in apache-airflow-providers-google==10.17.0. Consequently, we are using apache-airflow-providers-google>=10.17.0
+  # and skipping constraints installation, as the specified version does not meet our requirements.
   uv pip install "apache-airflow-providers-google>=10.17.0"
   uv pip install apache-airflow-providers-microsoft-azure --constraint /tmp/constraint.txt
 fi

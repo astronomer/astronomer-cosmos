b4f96f263f7b00fd43e4fb1fd6203a85311bf3ea
pankajastro <pankaj.singh@astronomer.io>
2024-11-25 18:46:16 +0530

    Add dbt-bigquery

--- scripts/test/integration-sqlite-setup.sh
+++ scripts/test/integration-sqlite-setup.sh
@@ -1,4 +1,4 @@
 pip uninstall -y dbt-core dbt-sqlite openlineage-airflow openlineage-integration-common; \
 rm -rf airflow.*; \
 airflow db init; \
-pip install 'dbt-core<1.8' 'dbt-sqlite' 'dbt-databricks' 'dbt-postgres' 'dbt-bigquery'
+pip install 'dbt-core<1.8' 'dbt-sqlite==1.5.0' 'dbt-databricks' 'dbt-postgres' 'dbt-bigquery'

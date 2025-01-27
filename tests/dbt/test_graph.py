ea946783b47215c25d54b08cc7e616fed1783dec
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-01-27 15:26:49 +0000

    Update test after changing dbt project

--- tests/dbt/test_graph.py
+++ tests/dbt/test_graph.py
@@ -1609,9 +1609,9 @@ def test_save_dbt_ls_cache(mock_variable_set, mock_datetime, tmp_dbt_project_dir
     hash_dir, hash_args = version.split(",")
     assert hash_args == "d41d8cd98f00b204e9800998ecf8427e"
     if sys.platform == "darwin":
-        assert hash_dir == "fa5edac64de49909d4b8cbc4dc8abd4f"
+        assert hash_dir == "679e32c775547b6600cd3171ca4d4083"
     else:
-        assert hash_dir == "9c9f712b6f6f1ace880dfc7f5f4ff051"
+        assert hash_dir == "bc2032c535d6ce6b39c19ee8bb98c436"
 
 
 @pytest.mark.integration

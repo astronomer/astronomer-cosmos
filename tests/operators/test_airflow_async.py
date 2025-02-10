0b05ab83d032a0c95eb20f9f1428278152f124e9
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-02-10 14:39:02 +0530

    Filter dbt and non-dbt kwrgs correctly for async operator (#1526)
    closes: https://github.com/astronomer/astronomer-cosmos/issues/1527

--- tests/operators/test_airflow_async.py
+++ tests/operators/test_airflow_async.py
@@ -1,3 +1,9 @@
+from datetime import datetime
+from pathlib import Path
+
+import pytest
+
+from cosmos import DbtDag, ExecutionConfig, ExecutionMode, ProfileConfig, ProjectConfig
 from cosmos.operators.airflow_async import (
     DbtBuildAirflowAsyncOperator,
     DbtCompileAirflowAsyncOperator,
@@ -18,6 +24,35 @@ from cosmos.operators.local import (
     DbtSourceLocalOperator,
     DbtTestLocalOperator,
 )
+from cosmos.profiles import get_automatic_profile_mapping
+
+DBT_PROJECTS_ROOT_DIR = Path(__file__).parent.parent.parent / "dev/dags/dbt"
+DBT_PROJECT_NAME = "original_jaffle_shop"
+
+
+@pytest.mark.integration
+def test_airflow_async_operator_init(mock_bigquery_conn):
+    """Test that Airflow can correctly parse an async operator with operator args"""
+    profile_mapping = get_automatic_profile_mapping(mock_bigquery_conn.conn_id, {})
+
+    profile_config = ProfileConfig(
+        profile_name="airflow_db",
+        target_name="bq",
+        profile_mapping=profile_mapping,
+    )
+
+    DbtDag(
+        project_config=ProjectConfig(dbt_project_path=DBT_PROJECTS_ROOT_DIR / DBT_PROJECT_NAME),
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            execution_mode=ExecutionMode.AIRFLOW_ASYNC,
+        ),
+        schedule_interval=None,
+        start_date=datetime(2023, 1, 1),
+        catchup=False,
+        dag_id="simple_dag_async",
+        operator_args={"location": "us", "install_deps": True},
+    )
 
 
 def test_dbt_build_airflow_async_operator_inheritance():

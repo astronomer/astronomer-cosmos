affc808f35cdc5a97ab7347a20ef0f4e150d8595
Prithvijit Guha <prithvijit_guha2@hotmail.com>
2025-02-22 22:40:26 +0530

    Removed repo specific content for duckdb

--- dev/dags/example_cosmos_dbt_duckdb.py
+++ dev/dags/example_cosmos_dbt_duckdb.py
@@ -1,5 +1,5 @@
 """
-An example DAG that uses Cosmos to render a dbt project into an Airflow DAG.
+An example DAG that uses Cosmos to render a duckdb-dbt project into an Airflow DAG.
 """
 
 import os

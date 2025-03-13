da29296a81889a3e8d31c10d262f487c7cf4ddf8
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-03-13 15:56:58 +0000

    Improve unit tests speed from 89s to 14s (#1600)
    A few tests, such as
    `test_configure_remote_target_path_no_remote_target`, were taking a long
    time when using `hatch run tests.py3.9-2.9:test-cov`.
    
    Time to run this command before these changes: 89.34s
    Time to run this command after these changes: 14.50s
    
    Also fix unittest that was failing locally.
    
    (cherry picked from commit d494dcdce3c67d7ce4ed92339951e4c3bdd578b4)

--- tests/dbt/test_graph.py
+++ tests/dbt/test_graph.py
@@ -1881,7 +1881,7 @@ def test_save_dbt_ls_cache(mock_variable_set, mock_datetime, tmp_dbt_project_dir
     hash_dir, hash_args = version.split(",")
     assert hash_args == "d41d8cd98f00b204e9800998ecf8427e"
     if sys.platform == "darwin":
-        assert hash_dir == "afbced719302d5b1efdfb191c617e349"
+        assert hash_dir == "391db5c7e1fb90214d829dd0476059a1"
     else:
         assert hash_dir == "0148da6f5f7fd260c9fa55c3b3c45168"
 

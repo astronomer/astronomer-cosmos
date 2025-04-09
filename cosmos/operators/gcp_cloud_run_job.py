d17225dddd2988da2d930628dbc7b3370f971cc2
John Horan <jhoran@zendesk.com>
2025-04-09 10:47:02 +0100

    forgot gcp

--- cosmos/operators/gcp_cloud_run_job.py
+++ cosmos/operators/gcp_cloud_run_job.py
@@ -86,10 +86,11 @@ class DbtGcpCloudRunJobBaseOperator(AbstractDbtBase, CloudRunExecuteJobOperator)
 
         default_args = kwargs.get("default_args", {})
         operator_kwargs = {**kwargs}
-        operator_args = {
-            *inspect.signature(CloudRunExecuteJobOperator.__init__).parameters.keys(),
-            *inspect.signature(BaseOperator.__init__).parameters.keys(),
-        }
+        operator_args = set()
+        for clazz in CloudRunExecuteJobOperator.__mro__:
+            operator_args.update(inspect.signature(clazz.__init__).parameters.keys())
+            if clazz == BaseOperator:
+                break
 
         base_kwargs = {}
         for arg in {*inspect.signature(AbstractDbtBase.__init__).parameters.keys()}:

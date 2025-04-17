5ae181e02e7696d82c51fb4f628abbdc25e41c8e
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-04-17 10:48:04 +0100

    Add documentation related to `copy_dbt_packages` configurations (#1671)
    Rename user-facing global configuration
    `default_copy_dbt_packages_value` to `default_copy_dbt_packages`.
    
    This is a follow-up to #1668, #1669 and #1670.

--- cosmos/config.py
+++ cosmos/config.py
@@ -147,7 +147,7 @@ class ProjectConfig:
 
     :param dbt_project_path: The path to the dbt project directory. Example: /path/to/dbt/project. Defaults to None
     :param install_dbt_deps: Run dbt deps during DAG parsing and task execution. Defaults to True.
-    :param copy_dbt_packages: Copy dbt_packages directory, if it exists, instead of creating a symbolic link. If not set, fetches the value from [cosmos]default_copy_dbt_packages_value (False by default).
+    :param copy_dbt_packages: Copy dbt_packages directory, if it exists, instead of creating a symbolic link. If not set, fetches the value from [cosmos]default_copy_dbt_packages (False by default).
     :param models_relative_path: The relative path to the dbt models directory within the project. Defaults to models
     :param seeds_relative_path: The relative path to the dbt seeds directory within the project. Defaults to seeds
     :param snapshots_relative_path: The relative path to the dbt snapshots directory within the project. Defaults to
@@ -168,7 +168,7 @@ class ProjectConfig:
 
     dbt_project_path: Path | None = None
     install_dbt_deps: bool = True
-    copy_dbt_packages: bool = settings.default_copy_dbt_packages_value
+    copy_dbt_packages: bool = settings.default_copy_dbt_packages
     manifest_path: Path | None = None
     models_path: Path | None = None
     seeds_path: Path | None = None
@@ -179,7 +179,7 @@ class ProjectConfig:
         self,
         dbt_project_path: str | Path | None = None,
         install_dbt_deps: bool = True,
-        copy_dbt_packages: bool = settings.default_copy_dbt_packages_value,
+        copy_dbt_packages: bool = settings.default_copy_dbt_packages,
         models_relative_path: str | Path = "models",
         seeds_relative_path: str | Path = "seeds",
         snapshots_relative_path: str | Path = "snapshots",

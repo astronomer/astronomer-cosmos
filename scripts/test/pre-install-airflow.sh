1023f645009b12e0dc55653f92308f9721790f26
Pankaj Koti <pankajkoti699@gmail.com>
2025-04-28 18:41:15 +0530

    Update scripts/test/pre-install-airflow.sh

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -26,7 +26,6 @@ mv /tmp/constraint.txt.tmp /tmp/constraint.txt
 pip install uv
 uv pip install pip --upgrade
 
-
 uv pip install "apache-airflow==$AIRFLOW_VERSION" --constraint /tmp/constraint.txt
 uv pip install apache-airflow-providers-docker --constraint /tmp/constraint.txt
 uv pip install apache-airflow-providers-postgres --constraint /tmp/constraint.txt

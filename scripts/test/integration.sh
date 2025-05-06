f0236ce6e54dc666ffe42fc8131ce62771275126
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-05-06 10:40:05 +0100

    Fix durations syntax

--- scripts/test/integration.sh
+++ scripts/test/integration.sh
@@ -17,7 +17,7 @@ pytest -vv \
     --cov=cosmos \
     --cov-report=term-missing \
     --cov-report=xml \
-    --durations \
+    --durations=0 \
     -m 'integration'  \
     --ignore=tests/perf \
     --ignore=tests/test_async_example_dag.py \

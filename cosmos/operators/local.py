5df10c72c7128b32877539f4682de27ee6ca162a
Pankaj Koti <pankajkoti699@gmail.com>
2025-05-20 15:15:12 +0530

    Add comment and tests

--- cosmos/operators/local.py
+++ cosmos/operators/local.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import inspect
 import json
-import logging
 import os
 import tempfile
 import time
@@ -478,7 +477,12 @@ class AbstractDbtLocalBase(AbstractDbtBase):
             self.profile_config.target_name,
         ]
         if self.invocation_mode == InvocationMode.DBT_RUNNER:
-            # TODO: Add comment on why we're doing this.
+            # PR #1484 introduced the use of dbtRunner during DAG parsing. As a result, invoking dbtRunner again
+            # during task execution can lead to task hangs—especially on Airflow 2.x. Investigation revealed that
+            # the issue stems from how dbtRunner handles static parsing. Cosmos copies the dbt project to temporary
+            # directories, and the use of different temp paths between parsing and execution appears to interfere
+            # with dbt's static parsing behavior. As a workaround, passing the --no-static-parser flag avoids these
+            # hangs and ensures reliable task execution.
             dbt_flags.append("--no-static-parser")
         return dbt_flags
 

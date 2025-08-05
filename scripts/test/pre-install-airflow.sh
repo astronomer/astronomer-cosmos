005ca0dea924e481f24ecc171f66106236bb3997
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-08-05 18:05:23 +0100

    Update scripts/test/pre-install-airflow.sh

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -1,4 +1,16 @@
-ython)
+#!/bin/bash
+
+set -v
+set -x
+set -e
+
+AIRFLOW_VERSION="$1"
+PYTHON_VERSION="$2"
+
+# Use this to set the appropriate Python environment in Github Actions,
+# while also not assuming --system when running locally.
+if [ "$GITHUB_ACTIONS" = "true" ] && [ -z "${VIRTUAL_ENV}" ]; then
+  py_path=$(which python)
   virtual_env_dir=$(dirname "$(dirname "$py_path")")
   export VIRTUAL_ENV="$virtual_env_dir"
 fi

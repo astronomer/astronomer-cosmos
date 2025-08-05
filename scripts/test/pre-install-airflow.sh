b125896808d2d8b952c35f01e8c191bd07463d91
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-08-05 18:04:33 +0100

    Remove unintentional change

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -20,7 +20,7 @@ mv /tmp/constraint.txt.tmp /tmp/constraint.txt
 pip install uv
 uv pip install pip --upgrade
 
-uv pip install "apache-airflow==$AIRFLOW_VERSION" apache-airflow-providers-docker apache-airflow-providers-postgres openlineage-airflow --constraint /tmp/constraint.txt
+uv pip install "apache-airflow==$AIRFLOW_VERSION" apache-airflow-providers-docker apache-airflow-providers-postgres --constraint /tmp/constraint.txt
 
 # Due to issue https://github.com/fsspec/gcsfs/issues/664
 uv pip install "gcsfs<2025.3.0"

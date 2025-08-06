e5623785dd3438a44ff44928970c9de4143d63b4
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-08-06 16:49:25 +0100

    Add dbt-core 1.10 to test matrix (#1767)
    Enable dbt 1.10in the test matrix.
    
    Pending:
    - The test failing in this PR ([test / Run-Integration-Tests (3.9, 3.0,
    1.10)
    (push)](https://github.com/astronomer/astronomer-cosmos/actions/runs/15760219653/job/44424889783?pr=1767)Failing
    after 1m) has the same error as the one failing in
    https://github.com/astronomer/astronomer-cosmos/issues/1817#issuecomment-2988320161
    - once that issue is solved, we can rebase this PR
    
    (cherry picked from commit 64893adc14e21a5c87043dc4b621e37c86d54631)

--- scripts/test/integration-setup.sh
+++ scripts/test/integration-setup.sh
@@ -21,7 +21,8 @@ airflow db reset -y
 AIRFLOW_VERSION=$(airflow version)
 AIRFLOW_MAJOR_VERSION=$(echo "$AIRFLOW_VERSION" | cut -d. -f1)
 if [ "$AIRFLOW_MAJOR_VERSION" -ge 3 ]; then
-    uv pip install cadwyn!=5.4.0
+  # https://github.com/zmievsa/cadwyn/issues/283
+    uv pip install "cadwyn>=5.4.1"
     echo "Detected Airflow $AIRFLOW_VERSION. Running 'airflow db migrate'..."
     airflow db migrate
 else
@@ -29,9 +30,11 @@ else
     airflow db init
 fi
 
-uv pip install -U "dbt-core==$DBT_VERSION" dbt-postgres dbt-bigquery dbt-vertica 'dbt-databricks' pyspark
-
-pip install  -U openlineage-airflow
+if [ "$DBT_VERSION" = "1.10" ]; then
+    uv pip install -U "dbt-core~=$DBT_VERSION" dbt-postgres dbt-bigquery dbt-vertica "dbt-databricks>=1.10.8" pyspark
+else
+    uv pip install -U "dbt-core~=$DBT_VERSION" dbt-postgres dbt-bigquery dbt-vertica dbt-databricks pyspark
+fi
 
 if python3 -c "import sys; print(sys.version_info >= (3, 9))" | grep -q 'True'; then
   pip install  'dbt-duckdb' "airflow-provider-duckdb>=0.2.0" "apache-airflow==$AIRFLOW_VERSION"

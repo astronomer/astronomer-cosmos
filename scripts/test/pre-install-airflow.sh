26fa573ec3e23fc3bba59c3213277b85a3a67e26
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-08-06 10:34:04 +0100

    Fix broken CI `tests.py3.11-2.8-1.9:test-integration-setup` (#1902)
    Installing `openlineage-airflow` and `airflow-provider-duckdb` without
    restricting the version of Airflow was leading to AF3 to be installed
    instead of AF2.8 when running
    
    ```bash
    hatch run tests.py3.11-2.8-1.9:test-integration-setup
    ```
    
    As can be seen in this GH action CI job run:
    
    https://github.com/astronomer/astronomer-cosmos/actions/runs/16731639151/job/47426561634?pr=1901
    
    Which led other PRs to fail:
    #1901
    
    Closes: #1905
    (cherry picked from commit ae453d5865b0cdd104334c8a056ada3792d9b663)

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -37,6 +37,7 @@ uv pip install "apache-airflow==$AIRFLOW_VERSION" apache-airflow-providers-docke
 # Due to issue https://github.com/fsspec/gcsfs/issues/664
 uv pip install "gcsfs<2025.3.0"
 
+
 if [ "$AIRFLOW_VERSION" = "2.4" ] || [ "$AIRFLOW_VERSION" = "2.5" ] || [ "$AIRFLOW_VERSION" = "2.6" ]  ; then
   uv pip install "apache-airflow-providers-amazon" "apache-airflow==$AIRFLOW_VERSION" "urllib3<2"
   uv pip install "apache-airflow-providers-cncf-kubernetes" "apache-airflow==$AIRFLOW_VERSION"

ed28dcdfa218eb238e2ddd42b08285f17518a462
Pankaj Koti <pankajkoti699@gmail.com>
2025-10-09 09:30:00 -0700

    Fix source manifest path type

--- cosmos/__init__.py
+++ cosmos/__init__.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 
 from cosmos import settings
 
-__version__ = "1.11.0a7"
+__version__ = "1.11.0a8"
 
 if not settings.enable_memory_optimised_imports:
     from cosmos.airflow.dag import DbtDag

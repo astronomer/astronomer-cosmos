015239779d5e2c5f25e862561ce831514258ece3
Anya Conti <anya.conti@spoileralert.com>
2025-10-28 10:24:38 -0400

    Fixing duplicate import from merging in main

--- tests/airflow/test_graph.py
+++ tests/airflow/test_graph.py
@@ -7,7 +7,6 @@ import pytest
 from airflow import __version__ as airflow_version
 from airflow.models import DAG
 
-from cosmos import DbtTestLocalOperator
 from cosmos.operators.watcher import DbtTestWatcherOperator
 
 try:

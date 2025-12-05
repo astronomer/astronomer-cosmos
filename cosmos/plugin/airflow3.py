fcea6647298e6d086a5159561749e655e486597e
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-12-05 18:29:28 +0530

    Register the listener in Airflow 3 plugin impl (#2187)
    closes: https://github.com/astronomer/astronomer-cosmos/issues/2182
    
    This PR registers the `dag_run_listener` module in the Airflow 3 plugin
    implementation to enable DAG run event tracking and telemetry for Cosmos
    tasks, addressing issue #2182.
    
    - Adds listener registration to `CosmosAF3Plugin` class
    - Adds test coverage to verify listener registration

--- cosmos/plugin/airflow3.py
+++ cosmos/plugin/airflow3.py
@@ -19,6 +19,7 @@ from fastapi.responses import HTMLResponse, JSONResponse, Response
 from packaging.version import Version
 
 from cosmos.constants import AIRFLOW_OBJECT_STORAGE_PATH_URL_SCHEMES
+from cosmos.listeners import dag_run_listener
 from cosmos.plugin.snippets import IFRAME_SCRIPT
 
 # Airflow version gating: External views feature for the plugins used here (CosmosAF3Plugin) exist only in >= 3.1
@@ -262,6 +263,8 @@ class CosmosAF3Plugin(AirflowPlugin):
 
     # Register external views for navigation
     external_views: list[dict[str, Any]] = []
+
+    listeners = [dag_run_listener]
 
     def __init__(self) -> None:
         super().__init__()

dec64533aeb83daee04b7c593ede604391a0e6d2
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-12-16 09:21:44 -0500

    Apply suggestion from @tatiana

--- cosmos/__init__.py
+++ cosmos/__init__.py
@@ -11,7 +11,6 @@ from cosmos import settings
 
 __version__ = "1.10.3"
 
-
 if not settings.enable_memory_optimised_imports:
     from cosmos.airflow.dag import DbtDag
     from cosmos.airflow.task_group import DbtTaskGroup

a147156daf33b9b027f505b714ec9dd89ad767a5
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-12-18 13:54:48 +0000

    Release 1.13.0a1

--- tests/test_example_dags.py
+++ tests/test_example_dags.py
@@ -25,7 +25,7 @@ MIN_VER_DAG_FILE: dict[str, list[str]] = {
     "2.8": ["cosmos_manifest_example.py", "simple_dag_async.py", "cosmos_callback_dag.py"],
 }
 
-IGNORED_DAG_FILES = ["performance_dag.py", "jaffle_shop_kubernetes.py"]
+IGNORED_DAG_FILES = ["performance_dag.py", "jaffle_shop_kubernetes.py", "jaffle_shop_watcher_kubernetes.py"]
 
 # Sort descending based on Versions and convert string to an actual version
 MIN_VER_DAG_FILE_VER: dict[Version, list[str]] = {

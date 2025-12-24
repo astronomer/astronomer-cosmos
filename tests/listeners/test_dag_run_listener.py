7cee4179469c3169f566751ebae32e22b240a202
Pankaj Koti <pankajkoti699@gmail.com>
2025-12-24 10:32:15 +0000

    Fix test validation error in dag_run_listener test

--- tests/listeners/test_dag_run_listener.py
+++ tests/listeners/test_dag_run_listener.py
@@ -260,6 +260,7 @@ def test_on_dag_run_failed_with_telemetry_metadata(mock_emit_usage_metrics_if_en
         execution_config=ExecutionConfig(invocation_mode=InvocationMode.DBT_RUNNER),
         render_config=RenderConfig(
             load_method=LoadMode.DBT_LS,
+            dbt_deps=False,
             test_behavior=TestBehavior.NONE,
             source_rendering_behavior=SourceRenderingBehavior.ALL,
         ),

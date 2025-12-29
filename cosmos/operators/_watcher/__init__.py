cd9d26ebc26ce74d60503e1d8c37b2f2cb8d9804
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-12-29 15:41:44 +0000

    Update cosmos/operators/_watcher/__init__.py

--- cosmos/operators/_watcher/__init__.py
+++ cosmos/operators/_watcher/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__all__ = ["get_xcom_val", "safe_xcom_push", "build_producer_state_fetcher", "WatcherTrigger", "_parse_compressed_xcom"]
+__all__ = ["get_xcom_val", "safe_xcom_push", "build_producer_state_fetcher", "WatcherTrigger"]
 
 from cosmos.operators._watcher.state import build_producer_state_fetcher, get_xcom_val, safe_xcom_push
 from cosmos.operators._watcher.triggerer import WatcherTrigger, _parse_compressed_xcom

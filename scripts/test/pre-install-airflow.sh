4023c46a0e4090599a5a23914b827f6e8ad6312e
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-01-14 15:56:04 +0000

    https://github.com/astronomer/astronomer-cosmos/actions/runs/21000539130/job/60369049173

--- scripts/test/pre-install-airflow.sh
+++ scripts/test/pre-install-airflow.sh
@@ -48,6 +48,7 @@ if [ "$AIRFLOW_VERSION" = "2.6" ]  ; then
   uv pip install "apache-airflow-providers-microsoft-azure" "apache-airflow==$AIRFLOW_VERSION"
   uv pip install "apache-airflow-providers-common-sql==1.12.0" "apache-airflow==$AIRFLOW_VERSION"
   uv pip install "pydantic<2.0"
+  uv pip install "pyparsing==2.4.7"
 elif [ "$AIRFLOW_VERSION" = "2.7" ] ; then
   uv pip install "apache-airflow-providers-amazon" --constraint /tmp/constraint.txt
   uv pip install "apache-airflow-providers-cncf-kubernetes" --constraint /tmp/constraint.txt

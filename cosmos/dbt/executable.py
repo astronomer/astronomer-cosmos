6a748bd92658d7f7b2aea1333ad5a43e0bb55add
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-01-14 14:58:33 +0000

    Error when users try to use invocation_mode incorrectly

--- cosmos/dbt/executable.py
+++ cosmos/dbt/executable.py
@@ -6,3 +6,15 @@ def get_system_dbt() -> str:
     Tries to identify which is the path to the dbt executable, return "dbt" otherwise.
     """
     return shutil.which("dbt") or "dbt"
+
+
+def is_dbt_installed_in_same_environment() -> bool:
+    """
+    Checks if dbt is installed in the same environment as the current one.
+    """
+    try:
+        import dbt  # noqa: F401
+    except ImportError:
+        return False
+    else:
+        return True

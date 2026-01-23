7bbdb4bdd8ea359ce82c855d8249b009081a885f
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-01-23 12:33:53 +0000

    Fix AF 2.8 test that was failing due to bug in AF 2.8.0
    INFO     airflow.models.taskinstance:taskinstance.py:1138 Marking task as FAILED. dag_id=watcher_dag, task_id=empty_model_run, execution_date=20260123T122719, start_date=, end_date=20260123T122721
    ERROR    cosmos.airflow.dag.DbtDag:dag.py:2881 Task failed; ti=<TaskInstance: watcher_dag.empty_model_run manual__2026-01-23T12:27:19.277635+00:00 [failed]>
    Traceback (most recent call last):
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/dag.py", line 2879, in test
        _run_task(ti=ti, inline_trigger=not triggerer_running, session=session)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/dag.py", line 4032, in _run_task
        ti._run_raw_task(session=session, raise_on_defer=inline_trigger)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/utils/session.py", line 76, in wrapper
        return func(*args, **kwargs)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/taskinstance.py", line 2335, in _run_raw_task
        self._execute_task_with_callbacks(context, test_mode, session=session)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/taskinstance.py", line 2500, in _execute_task_with_callbacks
        result = self._execute_task(context, task_orig)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/taskinstance.py", line 2517, in _execute_task
        return _execute_task(self, context, task_orig)
      File "/Users/tatiana.alchueyr/Library/Application Support/hatch/env/virtual/astronomer-cosmos/4lAAXoPP/tests.py3.10-2.8-1.11/lib/python3.10/site-packages/airflow/models/taskinstance.py", line 418, in _execute_task
        task_to_execute.execution_timeout - (timezone.utcnow() - task_instance.start_date)
    TypeError: unsupported operand type(s) for -: 'datetime.datetime' and 'NoneType'

--- tests/operators/test_watcher.py
+++ tests/operators/test_watcher.py
@@ -1086,7 +1086,10 @@ def test_dbt_dag_with_watcher_and_empty_model(caplog):
             invocation_mode=InvocationMode.DBT_RUNNER,
         ),
         render_config=RenderConfig(emit_datasets=False, test_behavior=TestBehavior.NONE),
-        operator_args={"trigger_rule": "all_success", "execution_timeout": timedelta(seconds=120)},
+        operator_args={
+            "trigger_rule": "all_success",
+            "execution_timeout": datetime.delta(seconds=120) if AIRFLOW_VERSION != Version("2.8") else None,
+        },
     )
     outcome = new_test_dag(watcher_dag)
     assert outcome.state == DagRunState.SUCCESS

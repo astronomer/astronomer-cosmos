68c8e2bdbe612d9aae4b2522fbf25ac1beadde28
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-02-10 10:27:08 +0000

    Attempt to solve integration tests https://github.com/astronomer/astronomer-cosmos/actions/runs/21860986265/job/63089817574

--- scripts/test/integration.sh
+++ scripts/test/integration.sh
@@ -13,14 +13,4 @@ airflow db check
 
 rm -rf dbt/jaffle_shop/dbt_packages;
 
-pytest -vv \
-    --cov=cosmos \
-    --cov-report=term-missing \
-    --cov-report=xml \
-    --durations=0 \
-    -m 'integration and not dbtfusion' \
-    --ignore=tests/perf \
-    --ignore=tests/test_async_example_dag.py \
-    --ignore=tests/test_example_k8s_dags.py \
-    --ignore=tests/operators/test_watcher_kubernetes_integration.py \
-    -k 'not (simple_dag_async or example_cosmos_python_models or example_virtualenv or jaffle_shop_kubernetes or jaffle_shop_watcher_kubernetes)'
+pytest -vv tests/operators/_asynchronous/test_bigquery.py

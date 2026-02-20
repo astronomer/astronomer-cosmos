1c03934eda481070e7f4e037f86fdff47135d513
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-02-20 11:40:41 +0000

    Apply suggestion from @tatiana

--- scripts/test/integration-dbt-async.sh
+++ scripts/test/integration-dbt-async.sh
@@ -16,6 +16,19 @@ echo "Installing dbt adapters for DBT_VERSION=$DBT_VERSION (<$NEXT_MINOR_VERSION
 pip uninstall dbt-adapters dbt-common dbt-core dbt-extractor dbt-postgres dbt-semantic-interfaces -y
 pip install -U "dbt-core>=$DBT_VERSION,<$NEXT_MINOR_VERSION" dbt-postgres dbt-databricks dbt-bigquery
 
+# apache-airflow-core 3.0.0 requires pydantic>=2.11.0, but the above dbt adapters in case of version 1.6 and 1.9 install
+# pydantic 1.10.22 which make it incompatible.
+# With pydantic 1.10.22 we get the below error
+# File "<...>/hatch/env/virtual/astronomer-cosmos/D9FI7Men/tests.py3.11-3.0-1.6/lib/python3.11/site-packages/cadwyn/_utils.py", line 5, in <module>
+#     from pydantic._internal._decorators import unwrap_wrapped_function
+# ModuleNotFoundError: No module named 'pydantic._internal'
+# Hence, we re-install pydantic with the required minimum version after installing dbt adapters.
+# dbt-core 1.9 raises
+if [ "$DBT_VERSION" = "1.6" ] || [ "$DBT_VERSION" = "1.9" ]; then
+    echo "DBT_VERSION is $DBT_VERSION, installing pydantic==2.11.0 for apache-airflow-core compatibility."
+    pip install "pydantic==2.11.0"
+fi
+
 # As on 28th April, 2025, the latest patch dbt-bigquery 1.6.13 on the 1.6 minor is not yet compatible with the latest
 # release of google-cloud-bigquery 3.31.0 as it tries to access a protected attribute which no longer exists in that
 # latest version for google-cloud-bigquery and gives the below error:

d716c05c5e1736b33d5d2177fea3131690ecb8d0
Evan Volgas <evolgas@gmail.com>
2026-02-23 05:58:34 -0800

    Address feedback from code review #2389 (#2394)
    Address the feedback from the https://github.com/astronomer/astronomer-cosmos/pull/2389 code review to help maintain the code in the future.
    
    (cherry picked from commit 78a4358987debf15269f190714ca2e9964a50953)

--- cosmos/dbt/selector.py
+++ cosmos/dbt/selector.py
@@ -168,6 +168,9 @@ class GraphSelector:
                 new_generation: set[str] = set()
                 for node_id in previous_generation:
                     if node_id not in processed_nodes:
+                        # When using dbt-loom for cross-project references, external nodes are filtered out
+                        # during manifest loading but local nodes may still reference them in depends_on.
+                        # Skip missing nodes to gracefully stop traversal at project boundaries.
                         if node_id in nodes:
                             new_generation.update(set(nodes[node_id].depends_on))
                         processed_nodes.add(node_id)
@@ -508,6 +511,9 @@ class NodeSelector:
         if self.config.graph_selectors:
             graph_selected_nodes = self.select_by_graph_operator()
             for node_id in graph_selected_nodes:
+                # When using dbt-loom for cross-project references, external nodes are filtered out
+                # during manifest loading but may be collected during graph traversal via depends_on.
+                # Skip these external node IDs that don't exist in the nodes dict.
                 if node_id not in self.nodes:
                     continue
                 node = self.nodes[node_id]

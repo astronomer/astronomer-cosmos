529b4b98eed3db349fda7207a107739bcad568c3
pankajastro <pankaj.singh@astronomer.io>
2026-03-11 11:51:32 +0530

    Add tests

--- tests/operators/_watcher/test_watcher_base.py
+++ tests/operators/_watcher/test_watcher_base.py
@@ -1,6 +1,8 @@
+from unittest.mock import Mock, patch
+
 import pytest
 
-from cosmos.operators._watcher.base import BaseConsumerSensor
+from cosmos.operators._watcher.base import BaseConsumerSensor, _process_dbt_log_event
 from cosmos.operators.local import DbtRunLocalOperator
 
 
@@ -53,3 +55,36 @@ class TestBaseConsumerSensor:
             project_dir="/tmp/sample_project",
         )
         assert sensor.extra_context == {}
+
+    @pytest.mark.parametrize(
+        "msg,should_push",
+        [
+            ("1 of 30 START sql table model bq_dev.a ......................... [RUN]", False),
+            ("6 of 30 FAIL creating sql view model bq_dev.stg_customers ............... [ERROR in 1.18s]", True),
+            ("6 of 30 ERROR creating sql view model bq_dev.stg_customers ..................... [ERROR in 1.18s]", True),
+        ],
+    )
+    def test_process_dbt_log_event_sensitive_words(self, msg, should_push):
+        task_instance = Mock()
+
+        dbt_log = {
+            "data": {
+                "node_info": {
+                    "unique_id": "model.test.my_model",
+                    "node_status": "None",
+                    "node_started_at": "2024-01-01T00:00:00",
+                    "node_finished_at": "2024-01-01T00:01:00",
+                },
+                "msg": msg,
+            },
+            "info": {},
+        }
+
+        with patch("cosmos.operators._watcher.base.safe_xcom_push") as mock_push:
+
+            _process_dbt_log_event(task_instance, dbt_log)
+
+            if should_push:
+                mock_push.assert_called_once()
+            else:
+                mock_push.assert_not_called()

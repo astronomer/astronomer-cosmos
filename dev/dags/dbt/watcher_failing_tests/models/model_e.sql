28307ad54059c3810deb944ee039f3b321fb9b4b
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-04-14 19:58:41 +0100

    Create dbt project & Airflow DAG to reproduce #2430

---
+++ dev/dags/dbt/watcher_failing_tests/models/model_e.sql
@@ -0,0 +1,5 @@
+select 1 as id, 'Alice' as first_name, 'US' as country, 95 as score, 'engineering' as department
+union all
+select 2, 'Bob', 'UK', 80, 'marketing'
+union all
+select 3, 'Charlie', 'CA', 70, 'engineering'

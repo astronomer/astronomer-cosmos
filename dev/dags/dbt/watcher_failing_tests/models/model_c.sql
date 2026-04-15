f655c9d8fe3036c37cc354733494c8fe5341ce84
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2026-04-15 11:46:04 +0100

    Simplify reproduction of problem

--- dev/dags/dbt/watcher_failing_tests/models/model_c.sql
+++
@@ -1,5 +0,0 @@
-select 1 as id, 'alice@example.com' as email, 'US' as country, 'engineering' as department
-union all
-select 2, 'bob@example.com', 'UK', 'marketing'
-union all
-select 3, 'charlie@example.com', 'CA', 'engineering'

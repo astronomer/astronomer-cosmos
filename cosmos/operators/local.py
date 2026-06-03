46a4952b95e554b25b795e53a7d6b3644bd206e9
goingforstudying-ctrl <goingforstudying@ctrl.com>
2026-06-03 04:46:11 +0000

    refactor: extract warning helper to fix ruff C901 complexity

--- cosmos/operators/local.py
+++ cosmos/operators/local.py
@@ -935,8 +935,20 @@ class AbstractDbtLocalBase(AbstractDbtBase):
                 self.subprocess_hook.send_sigterm()
 
 
-class DbtLocalBaseOperator(AbstractDbtLocalBase, BaseOperator):
-    template_fields: Sequence[str] = AbstractDbtLocalBase.template_fields
+def _warn_on_output_only_fields(kwargs: dict[str, Any], class_name: str) -> None:
+    """Warn when output-only template fields are passed directly to local operators."""
+    for field in _OUTPUT_ONLY_TEMPLATE_FIELDS:
+        if field in kwargs:
+            logger.warning(
+                "The '%s' argument passed to %s will be overwritten at runtime; "
+                "it is an output-only template field.",
+                field,
+                class_name,
+            )
+
+
+class DbtLocalBaseOperator(AbstractDbtLocalBase, BaseOperator):  # type: ignore[misc]
+    template_fields: Sequence[str] = AbstractDbtLocalBase.template_fields  # type: ignore[operator]
 
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         # In PR #1474, we refactored cosmos.operators.base.AbstractDbtBase to remove its inheritance from BaseOperator
@@ -951,15 +963,7 @@ class DbtLocalBaseOperator(AbstractDbtLocalBase, BaseOperator):
 
         default_args = kwargs.get("default_args", {})
 
-        # Warn when output-only template fields are passed directly to local operators
-        for field in _OUTPUT_ONLY_TEMPLATE_FIELDS:
-            if field in kwargs:
-                logger.warning(
-                    "The '%s' argument passed to %s will be overwritten at runtime; "
-                    "it is an output-only template field.",
-                    field,
-                    self.__class__.__name__,
-                )
+        _warn_on_output_only_fields(kwargs, self.__class__.__name__)
 
         for arg in operator_args:
             try:
